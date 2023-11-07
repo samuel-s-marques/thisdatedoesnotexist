646657abe4e35408101a546f5688feea2ea94366
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-07 17:51:51 -0300

    add variables

--- lib/app/features/auth/store/auth_store.dart
+++ lib/app/features/auth/store/auth_store.dart
@@ -1,7 +1,18 @@
+import 'package:flutter/cupertino.dart';
 import 'package:mobx/mobx.dart';
 
 part 'auth_store.g.dart';
 
 class AuthStore = _AuthStoreBase with _$AuthStore;
 
-abstract class _AuthStoreBase with Store {}
+abstract class _AuthStoreBase with Store {
+  @observable
+  bool isLoading = false;
+
+  @observable
+  bool obscurePassword = true;
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController repeatPasswordController = TextEditingController();
+}
