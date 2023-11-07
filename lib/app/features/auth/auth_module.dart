8e8b9bd9627c57de121e545ab7e2158a36976c12
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-07 18:01:18 -0300

    add lazySingleton

--- lib/app/features/auth/auth_module.dart
+++ lib/app/features/auth/auth_module.dart
@@ -1,8 +1,11 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:thisdatedoesnotexist/app/features/auth/store/auth_store.dart';
 
 class AuthModule extends Module {
   @override
-  void binds(i) {}
+  void binds(i) {
+    i.addLazySingleton((i) => AuthStore());
+  }
 
   @override
   void routes(r) {}
