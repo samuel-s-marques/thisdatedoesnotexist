a9db6a9daa5359aa4ab6c8aed2e0a9a69b3750ba
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-07 18:18:05 -0300

    add store

--- lib/app/features/auth/pages/auth_page.dart
+++ lib/app/features/auth/pages/auth_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:thisdatedoesnotexist/app/features/auth/store/auth_store.dart';
 
 class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
@@ -8,6 +9,8 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
+  AuthStore store = AuthStore();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
