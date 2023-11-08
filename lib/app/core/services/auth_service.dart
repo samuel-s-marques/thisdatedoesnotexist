5ab7274d0bf592a1b065101797c7d4017394f13a
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-08 15:46:08 -0300

    Create auth_service.dart

---
+++ lib/app/core/services/auth_service.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:thisdatedoesnotexist/app/core/enum/auth_status_enum.dart';
+import 'package:thisdatedoesnotexist/app/core/exceptions/auth_exception.dart';
+import 'package:thisdatedoesnotexist/app/core/util.dart';
+
+class AuthService {
+  final _auth = FirebaseAuth.instance;
+
+  Future<AuthStatus> createAccount({required String email, required String password}) async {
+    AuthStatus status = AuthStatus.unknown;
+
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email.trim(),
+        password: password,
+      );
+
+      _auth.currentUser!.updateDisplayName(email.toUsername());
+      status = AuthStatus.successful;
+    } on FirebaseAuthException catch (exception) {
+      status = AuthExceptionHandler.handleAuthException(exception);
+    }
+
+    return status;
+  }
+}
