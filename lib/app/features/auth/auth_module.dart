f03fe19870a05ff34dadab8df2e446ffaa0ff6a9
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-08 17:31:09 -0300

    add `forgot password` route

--- lib/app/features/auth/auth_module.dart
+++ lib/app/features/auth/auth_module.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:thisdatedoesnotexist/app/features/auth/pages/auth_page.dart';
+import 'package:thisdatedoesnotexist/app/features/auth/pages/forgot_password_page.dart';
 import 'package:thisdatedoesnotexist/app/features/auth/store/auth_store.dart';
 
 class AuthModule extends Module {
@@ -11,5 +12,6 @@ class AuthModule extends Module {
   @override
   void routes(r) {
     r.child('/', child: (context) => AuthPage());
+    r.child('/forgot', child: (context) => ForgotPasswordPage());
   }
 }
