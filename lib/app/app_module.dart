8c273d1c370583d3f2aae6e98df6c637107064cd
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-08 01:55:24 -0300

    add `AuthStore` binding

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,9 +1,15 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:thisdatedoesnotexist/app/features/auth/auth_module.dart';
+import 'package:thisdatedoesnotexist/app/features/auth/store/auth_store.dart';
 
 class AppModule extends Module {
   @override
-  void binds(i) {}
+  void binds(i) {
+    i.add((i) => AuthStore());
+  }
 
   @override
-  void routes(r) {}
+  void routes(r) {
+    r.module('/', module: AuthModule());
+  }
 }
