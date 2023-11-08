211437ad3885f4beb5ec07bec09fb0fc244a993d
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-08 17:31:17 -0300

    Create forgot_password_page.dart

---
+++ lib/app/features/auth/pages/forgot_password_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:thisdatedoesnotexist/app/features/auth/store/auth_store.dart';
+
+class ForgotPasswordPage extends StatefulWidget {
+  const ForgotPasswordPage({super.key});
+
+  @override
+  State<ForgotPasswordPage> createState() => _ForgotPasswordPageState();
+}
+
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  AuthStore store = AuthStore();
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    store.emailController = TextEditingController();
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Form(
+          child: Observer(
+            builder: (_) => Wrap(
+              children: [
+                TextFormField(
+                  controller: store.emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(labelText: 'E-mail'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'E-mail is required.';
+                    }
+
+                    final RegExp emailPattern = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+                    if (!emailPattern.hasMatch(value)) {
+                      return 'A valid email address is required';
+                    }
+
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 40,
+                  width: double.infinity,
+                  child: Observer(
+                    builder: (_) => ElevatedButton(
+                      onPressed: () {
+                        if (!store.isLoading && formKey.currentState!.validate()) {
+                          store.forgotPassword(context);
+                        }
+                      },
+                      child: store.isLoading
+                          ? const CircularProgressIndicator()
+                          : const Text('Send reset password e-mail'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
