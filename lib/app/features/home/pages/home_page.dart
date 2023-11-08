00c407289a5e859cd4200c8bd2552963abb6356b
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-08 19:02:42 -0300

    add pages

--- lib/app/features/home/pages/home_page.dart
+++ lib/app/features/home/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:thisdatedoesnotexist/app/features/home/store/home_store.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,26 +10,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  HomeStore store = HomeStore();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat_bubble),
-            label: 'Chat',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'User',
-          ),
-        ],
+      body: Observer(builder: (_) => [Column(), ...store.pages].elementAt(store.selectedIndex)),
+      bottomNavigationBar: Observer(
+        builder: (_) => BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat_bubble),
+              label: 'Chat',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: store.selectedIndex,
+          onTap: store.setIndex,
+        ),
       ),
     );
   }
