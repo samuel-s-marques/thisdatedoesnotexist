0fa18f9183ac583157f4b57a6123170539cb4080
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-08 19:02:47 -0300

    add pages

--- lib/app/features/home/store/home_store.dart
+++ lib/app/features/home/store/home_store.dart
@@ -1,7 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:mobx/mobx.dart';
 
 part 'home_store.g.dart';
 
 class HomeStore = HomeStoreBase with _$HomeStore;
 
-abstract class HomeStoreBase with Store {}
+abstract class HomeStoreBase with Store {
+  List<Widget> pages = [
+    Text("teste"),
+    Text("teste 2"),
+  ];
+
+  @observable
+  int selectedIndex = 0;
+
+  @action
+  void setIndex(int index) {
+    selectedIndex = index;
+  }
+}
