6384b685b09d5b50ba444e42cfa1243e4b247837
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-09 20:38:50 -0300

    initialize sentry

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,21 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:thisdatedoesnotexist/app/app_module.dart';
 import 'package:thisdatedoesnotexist/app/app_widget.dart';
 import 'package:thisdatedoesnotexist/firebase_options.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  runApp(
-    ModularApp(
-      module: AppModule(),
-      child: AppWidget(),
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = String.fromEnvironment('SENTRY_DSN');
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(
+      ModularApp(
+        module: AppModule(),
+        child: AppWidget(),
+      ),
     ),
   );
 }
