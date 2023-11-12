960aa3278a14b8a986d721fd0b17d19a1383eef9
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-12 14:02:09 -0300

    replace `sectionwidget` with `settingsection`

--- lib/app/features/settings/pages/settings_page.dart
+++ lib/app/features/settings/pages/settings_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:thisdatedoesnotexist/app/features/profile/widgets/section_widget.dart';
+import 'package:thisdatedoesnotexist/app/features/settings/widgets/settings_section.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -20,81 +20,76 @@ class _SettingsPageState extends State<SettingsPage> {
           ),
         ],
       ),
-      body: ListView(
-        children: [
-          SectionWidget(
-            title: 'Account Settings',
-            content: ListView(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              children: [
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 15),
+        child: Column(
+          children: [
+            SettingsSection(
+              title: 'Account Settings',
+              content: [
                 ListTile(
                   title: Text('Change Password'),
                   leading: Icon(Icons.lock_person),
                   trailing: Icon(Icons.arrow_forward_ios),
+                  contentPadding: EdgeInsets.zero,
                 ),
                 ListTile(
                   title: Text('Delete Account'),
                   leading: Icon(Icons.delete),
                   trailing: Icon(Icons.arrow_forward_ios),
+                  contentPadding: EdgeInsets.zero,
                 ),
               ],
             ),
-          ),
-          SectionWidget(
-            title: 'Notification',
-            content: ListView(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              children: [
+            SettingsSection(
+              title: 'Notification',
+              content: [
                 ListTile(
                   title: Text('Push Notifications'),
                   leading: Icon(Icons.notifications),
                   trailing: Icon(Icons.arrow_forward_ios),
+                  contentPadding: EdgeInsets.zero,
                 ),
                 ListTile(
                   title: Text('E-mail Notifications'),
                   leading: Icon(Icons.email),
                   trailing: Icon(Icons.arrow_forward_ios),
+                  contentPadding: EdgeInsets.zero,
                 ),
               ],
             ),
-          ),
-          SectionWidget(
-            title: 'Security',
-            content: ListView(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              children: [
+            SettingsSection(
+              title: 'Security',
+              content: [
                 ListTile(
                   title: Text('Two-Factor Authentication'),
                   leading: Icon(Icons.lock),
                   trailing: Icon(Icons.arrow_forward_ios),
+                  contentPadding: EdgeInsets.zero,
                 ),
                 ListTile(
                   title: Text('Account Linked Services'),
                   leading: Icon(Icons.link),
                   trailing: Icon(Icons.arrow_forward_ios),
+                  contentPadding: EdgeInsets.zero,
                 ),
               ],
             ),
-          ),
-          SectionWidget(
-            title: 'App Preferences',
-            content: ListView(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              children: [
+            SettingsSection(
+              title: 'App Preferences',
+              content: [
                 ListTile(
                   title: Text('Language'),
                   leading: Icon(Icons.language),
                   subtitle: Text('English'),
                   trailing: Icon(Icons.arrow_forward_ios),
+                  contentPadding: EdgeInsets.zero,
                 ),
                 ListTile(
                   title: Text('Theme'),
                   leading: Icon(Icons.colorize),
                   subtitle: Text('Light'),
+                  contentPadding: EdgeInsets.zero,
                   trailing: Switch(
                     value: false,
                     onChanged: (value) {},
@@ -102,46 +97,42 @@ class _SettingsPageState extends State<SettingsPage> {
                 ),
               ],
             ),
-          ),
-          SectionWidget(
-            title: 'Help and Support',
-            content: ListView(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              children: [
+            SettingsSection(
+              title: 'Help and Support',
+              content: [
                 ListTile(
                   title: Text('FAQs'),
                   leading: Icon(Icons.help),
                   trailing: Icon(Icons.arrow_forward_ios),
+                  contentPadding: EdgeInsets.zero,
                 ),
                 ListTile(
                   title: Text('Feedback'),
                   leading: Icon(Icons.feedback),
                   trailing: Icon(Icons.arrow_forward_ios),
+                  contentPadding: EdgeInsets.zero,
                 ),
                 ListTile(
                   title: Text('Contact Support'),
                   leading: Icon(Icons.contact_support),
                   trailing: Icon(Icons.arrow_forward_ios),
+                  contentPadding: EdgeInsets.zero,
                 ),
               ],
             ),
-          ),
-          SectionWidget(
-            title: 'Legal and Terms',
-            content: ListView(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              children: [
+            SettingsSection(
+              title: 'Legal and Terms',
+              content: [
                 ListTile(
                   title: Text('Terms of Service and Privacy Policy'),
                   leading: Icon(Icons.policy),
                   trailing: Icon(Icons.arrow_forward_ios),
+                  contentPadding: EdgeInsets.zero,
                 ),
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
