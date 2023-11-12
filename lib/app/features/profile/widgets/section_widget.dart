b8a8210f873ee6237432f39d9be89cb1f8f0167a
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-12 13:23:11 -0300

    add padding

--- lib/app/features/profile/widgets/section_widget.dart
+++ lib/app/features/profile/widgets/section_widget.dart
@@ -3,22 +3,27 @@ import 'package:flutter/cupertino.dart';
 class SectionWidget extends StatelessWidget {
   final String title;
   final Widget content;
+  final EdgeInsetsGeometry? padding;
 
   const SectionWidget({
     super.key,
     required this.title,
     required this.content,
+    this.padding = const EdgeInsets.only(bottom: 10),
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Text(title),
-        content,
-      ],
+    return Padding(
+      padding: padding ?? const EdgeInsets.only(bottom: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(title),
+          content,
+        ],
+      ),
     );
   }
 }
