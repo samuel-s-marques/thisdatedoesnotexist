a760af5582eeab3100670dd0fd12642c23697a2e
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-12 14:02:13 -0300

    Create settings_tile.dart

---
+++ lib/app/features/settings/widgets/settings_tile.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SettingsTile extends StatelessWidget {
+  final String title;
+  final String? subtitle;
+  final GestureTapCallback? onTap;
+  final Widget? leading;
+  final Widget? trailing;
+
+  const SettingsTile({
+    super.key,
+    required this.title,
+    this.subtitle,
+    this.onTap,
+    this.leading,
+    this.trailing,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: leading,
+      trailing: trailing,
+      title: Text(title),
+      contentPadding: EdgeInsets.zero,
+      subtitle: subtitle != null ? Text(subtitle!) : null,
+      onTap: onTap,
+    );
+  }
+}
