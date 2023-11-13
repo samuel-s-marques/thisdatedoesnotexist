2de0fcc75319a3ebe32b8644c874fa1074598a8c
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-13 15:00:40 -0300

    add title

--- lib/app/features/settings/pages/help_and_support_page.dart
+++ lib/app/features/settings/pages/help_and_support_page.dart
@@ -8,7 +8,9 @@ class HelpAndSupportPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Help & Support'),
+      ),
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 15),
         children: [
