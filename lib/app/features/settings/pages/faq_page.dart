2af77afffe25941e388ff76b72e3771e791d2c1a
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-13 14:48:48 -0300

    add searchable faq

--- lib/app/features/settings/pages/faq_page.dart
+++ lib/app/features/settings/pages/faq_page.dart
@@ -2,11 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:thisdatedoesnotexist/app/features/settings/store/settings_store.dart';
 
-class FaqPage extends StatelessWidget {
-  FaqPage({super.key});
+class FaqPage extends StatefulWidget {
+  const FaqPage({super.key});
 
+  @override
+  State<FaqPage> createState() => _FaqPageState();
+}
+
+class _FaqPageState extends State<FaqPage> {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   final SettingsStore store = SettingsStore();
+
+  @override
+  void initState() {
+    store.searchFaqQuery = '';
+    store.filteredFaq.clear();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,14 +32,52 @@ class FaqPage extends StatelessWidget {
             Form(
               key: formKey,
               child: TextFormField(
+                onChanged: store.searchFaq,
                 decoration: const InputDecoration(
                   hintText: 'Search',
                   prefixIcon: Icon(Icons.search),
                 ),
               ),
             ),
-            Observer(
-              builder: (_) => ListView.builder(
+            Observer(builder: (_) {
+              if (store.filteredFaq.isNotEmpty || store.searchFaqQuery.isNotEmpty) {
+                if (store.filteredFaq.isEmpty) {
+                  return const Center(
+                    child: Text('No results found.'),
+                  );
+                } else {
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: store.filteredFaq.length,
+                    padding: EdgeInsets.zero,
+                    itemBuilder: (context, index) {
+                      return ExpansionTile(
+                        tilePadding: EdgeInsets.zero,
+                        title: Text(
+                          store.filteredFaq.elementAt(index).key,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 15, bottom: 15),
+                            child: Text(
+                              store.filteredFaq.elementAt(index).value,
+                              style: const TextStyle(
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+              }
+
+              return ListView.builder(
                 shrinkWrap: true,
                 physics: const NeverScrollableScrollPhysics(),
                 itemCount: store.faq.length,
@@ -54,8 +104,8 @@ class FaqPage extends StatelessWidget {
                     ],
                   );
                 },
-              ),
-            ),
+              );
+            }),
           ],
         ),
       ),
