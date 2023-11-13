4d336f4e007808084d71ef362e8e3f9798f344ba
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-13 00:31:31 -0300

    Create account_information_page.dart

---
+++ lib/app/features/settings/pages/account_information_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AccountInformationPage extends StatelessWidget {
+  const AccountInformationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
