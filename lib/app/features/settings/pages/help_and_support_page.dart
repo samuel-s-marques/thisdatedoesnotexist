3c17982a3212c2f133e840f353563899182cf382
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-15 22:52:59 -0300

    implement feedback

--- lib/app/features/settings/pages/help_and_support_page.dart
+++ lib/app/features/settings/pages/help_and_support_page.dart
@@ -1,9 +1,19 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:dio/dio.dart';
+import 'package:feedback/feedback.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:thisdatedoesnotexist/app/core/util.dart';
+import 'package:thisdatedoesnotexist/app/features/settings/store/settings_store.dart';
 import 'package:thisdatedoesnotexist/app/features/settings/widgets/settings_tile.dart';
 
 class HelpAndSupportPage extends StatelessWidget {
-  const HelpAndSupportPage({super.key});
+  HelpAndSupportPage({super.key});
+
+  final SettingsStore store = SettingsStore();
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +42,35 @@ class HelpAndSupportPage extends StatelessWidget {
           SettingsTile(
             title: 'Feedback',
             trailing: const Icon(Icons.arrow_forward_ios),
-            onTap: () {},
+            onTap: () {
+              BetterFeedback.of(context).show((UserFeedback feedback) async {
+                try {
+                  final Directory directory = await path_provider.getTemporaryDirectory();
+                  final Uint8List screenshot = feedback.screenshot;
+                  await File('${directory.path}/screenshot.png').writeAsBytes(screenshot);
+
+                  final Response<dynamic> response = await Dio().post(
+                    '${store.server}/api/feedback',
+                    data: FormData.fromMap({
+                      'user_uid': store.authService.getUser().uid,
+                      'text': feedback.text,
+                      'screenshot': await MultipartFile.fromFile(
+                        '${directory.path}/screenshot.png',
+                        filename: 'screenshot.png',
+                      ),
+                    }),
+                  );
+
+                  if (response.statusCode == 201) {
+                    context.showSnackBarSuccess(message: 'Feedback sent successfully.');
+                  } else {
+                    context.showSnackBarError(message: 'Error in server while sending feedback. Try again later.');
+                  }
+                } catch (e) {
+                  context.showSnackBarError(message: 'Error sending feedback. Try again later.');
+                }
+              });
+            },
           ),
           SettingsTile(
             title: 'About us',
