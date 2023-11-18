a832c9b3502aa8a5c05bb58c9be94a996dd7cde2
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-17 23:38:22 -0300

    add uuid

--- lib/app/features/home/models/character_model.dart
+++ lib/app/features/home/models/character_model.dart
@@ -1,10 +1,12 @@
 class CharacterModel {
   CharacterModel({
+    required this.uuid,
     required this.name,
     required this.surname,
     required this.age,
   });
 
+  final String uuid;
   final String name;
   final String surname;
   final int age;
