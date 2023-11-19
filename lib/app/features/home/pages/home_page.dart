d828bec2e3acf5e78165c54f0c56834d667571d0
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-18 23:17:39 -0300

    add FutureBuilder

--- lib/app/features/home/pages/home_page.dart
+++ lib/app/features/home/pages/home_page.dart
@@ -23,7 +23,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-    store.getTodayCards();
     UserModel(uid: store.authService.getUser().uid).getSwipes().then((swipes) => store.setSwipes(swipes));
     super.initState();
   }
@@ -44,49 +43,64 @@ class _HomePageState extends State<HomePage> {
         builder: (_) => [
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 15),
-            child: Column(
-              children: [
-                Flexible(
-                  child: CardSwiper(
-                    padding: EdgeInsets.zero,
-                    cardsCount: store.cards.length,
-                    controller: store.cardSwiperController,
-                    allowedSwipeDirection: AllowedSwipeDirection.only(left: true, right: true),
-                    isLoop: false,
-                    onSwipe: store.onSwipe,
-                    cardBuilder: (
-                      BuildContext context,
-                      int index,
-                      int percentThresholdX,
-                      int percentThresholdY,
-                    ) {
-                      return CardWidget(character: store.cards[index]);
-                    },
+            child: FutureBuilder(
+              future: store.getTodayCards(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  store.cards = snapshot.data;
+
+                  return Column(
+                    children: [
+                      Flexible(
+                        child: CardSwiper(
+                          padding: EdgeInsets.zero,
+                          cardsCount: store.cards.length,
+                          controller: store.cardSwiperController,
+                          allowedSwipeDirection: AllowedSwipeDirection.only(left: true, right: true),
+                          isLoop: false,
+                          onSwipe: store.onSwipe,
+                          cardBuilder: (
+                            BuildContext context,
+                            int index,
+                            int percentThresholdX,
+                            int percentThresholdY,
+                          ) {
+                            return CardWidget(character: store.cards[index]);
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          IconButton(
+                            onPressed: () => store.cardSwiperController.swipeLeft(),
+                            color: Colors.white,
+                            style: IconButton.styleFrom(
+                              backgroundColor: Colors.red,
+                            ),
+                            icon: const Icon(Icons.close),
+                          ),
+                          IconButton(
+                            onPressed: () => store.cardSwiperController.swipeRight(),
+                            color: Colors.white,
+                            style: IconButton.styleFrom(
+                              backgroundColor: Colors.green,
+                            ),
+                            icon: const Icon(Icons.check),
+                          ),
+                        ],
+                      ),
+                    ],
+                  );
+                }
+
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.blue,
                   ),
-                ),
-                const SizedBox(height: 15),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      onPressed: () => store.cardSwiperController.swipeLeft(),
-                      color: Colors.white,
-                      style: IconButton.styleFrom(
-                        backgroundColor: Colors.red,
-                      ),
-                      icon: const Icon(Icons.close),
-                    ),
-                    IconButton(
-                      onPressed: () => store.cardSwiperController.swipeRight(),
-                      color: Colors.white,
-                      style: IconButton.styleFrom(
-                        backgroundColor: Colors.green,
-                      ),
-                      icon: const Icon(Icons.check),
-                    ),
-                  ],
-                ),
-              ],
+                );
+              },
             ),
           ),
           ...store.pages
