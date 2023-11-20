8a072b34a0855b05d0794499d93b0e139a2a359c
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-20 18:47:14 -0300

    remove pages

--- lib/app/features/onboarding/store/onboarding_store.dart
+++ lib/app/features/onboarding/store/onboarding_store.dart
@@ -1,29 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:mobx/mobx.dart';
+import 'package:thisdatedoesnotexist/app/core/models/hobby_model.dart';
 
 part 'onboarding_store.g.dart';
 
 class OnboardingStore = OnboardingStoreBase with _$OnboardingStore;
 
 abstract class OnboardingStoreBase with Store {
-  @observable
-  ObservableList<PageViewModel> pages = ObservableList.of([
-    PageViewModel(
-      title: 'Page one',
-      bodyWidget: Column(
-        children: [
-          Text("Page one"),
-        ],
-      ),
-    ),
-    PageViewModel(
-      title: 'Page two',
-      bodyWidget: Column(
-        children: [
-          Text("Page two"),
-        ],
-      ),
-    ),
-  ]);
+  Future<List<Hobby>> getHobbies() async {
+    return [];
+  }
 }
