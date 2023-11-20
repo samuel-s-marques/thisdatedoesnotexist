c7696920822d54cc21fc7ac27c9027f0e983f9e7
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-19 23:19:01 -0300

    replace controller with stor

---
+++ lib/app/features/onboarding/store/onboarding_store.dart
@@ -0,0 +1,7 @@
+import 'package:mobx/mobx.dart';
+
+part 'onboarding_store.g.dart';
+
+class OnboardingStore = OnboardingStoreBase with _$OnboardingStore;
+
+abstract class OnboardingStoreBase with Store {}
