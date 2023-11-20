2fa68bf931ec0c9e3dbad882a710d58d54338fa0
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-20 18:47:08 -0300

    add pages and hobbies & interests

--- lib/app/features/onboarding/pages/onboarding_page.dart
+++ lib/app/features/onboarding/pages/onboarding_page.dart
@@ -10,7 +10,30 @@ class OnboardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IntroductionScreen(
-      pages: store.pages,
+      pages: [
+        PageViewModel(
+          title: 'Page one',
+          bodyWidget: Column(
+            children: [
+              Text("Page one"),
+            ],
+          ),
+        ),
+        PageViewModel(
+          title: 'Hobbies & Interests',
+          bodyWidget: Column(
+            children: [
+              const Text('Pick up to 4 things you do or you have interest in doing. It will help you match with characters with similar interests.'),
+              FutureBuilder(
+                future: store.getHobbies(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  return Container();
+                },
+              ),
+            ],
+          ),
+        ),
+      ],
       showNextButton: false,
       showDoneButton: false,
     );
