124116204179c69c0a0bdbeac58b300aac175a30
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-20 12:51:23 -0300

    add sample pages

--- lib/app/features/onboarding/store/onboarding_store.dart
+++ lib/app/features/onboarding/store/onboarding_store.dart
@@ -1,7 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 import 'package:mobx/mobx.dart';
 
 part 'onboarding_store.g.dart';
 
 class OnboardingStore = OnboardingStoreBase with _$OnboardingStore;
 
-abstract class OnboardingStoreBase with Store {}
+abstract class OnboardingStoreBase with Store {
+  @observable
+  ObservableList<PageViewModel> pages = ObservableList.of([
+    PageViewModel(
+      title: 'Page one',
+      bodyWidget: Column(
+        children: [
+          Text("Page one"),
+        ],
+      ),
+    ),
+    PageViewModel(
+      title: 'Page two',
+      bodyWidget: Column(
+        children: [
+          Text("Page two"),
+        ],
+      ),
+    ),
+  ]);
+}
