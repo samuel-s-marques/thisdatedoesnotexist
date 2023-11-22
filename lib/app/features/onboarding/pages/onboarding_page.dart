a1e24582df180b0cdcac18bee1d7bec60aae5687
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-22 17:43:46 -0300

    add list of hobbies

--- lib/app/features/onboarding/pages/onboarding_page.dart
+++ lib/app/features/onboarding/pages/onboarding_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:thisdatedoesnotexist/app/core/models/hobby_model.dart';
+import 'package:thisdatedoesnotexist/app/core/util.dart';
 import 'package:thisdatedoesnotexist/app/features/onboarding/store/onboarding_store.dart';
 
 class OnboardingPage extends StatelessWidget {
@@ -22,12 +26,51 @@ class OnboardingPage extends StatelessWidget {
         PageViewModel(
           title: 'Hobbies & Interests',
           bodyWidget: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const Text('Pick up to 4 things you do or you have interest in doing. It will help you match with characters with similar interests.'),
+              const SizedBox(height: 15),
               FutureBuilder(
                 future: store.getHobbies(),
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  return Container();
+                  if (snapshot.hasData) {
+                    final Map<String, List<Hobby>> groupedHobbies = snapshot.data;
+
+                    return Observer(
+                      builder: (_) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: groupedHobbies.keys.map((String key) {
+                            return Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  key.capitalize(),
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Wrap(
+                                  spacing: 5,
+                                  children: groupedHobbies[key]!.map((Hobby hobby) {
+                                    return ActionChip(
+                                      label: Text(hobby.name),
+                                    );
+                                  }).toList(),
+                                ),
+                                const SizedBox(height: 15),
+                              ],
+                            );
+                          }).toList(),
+                        );
+                      },
+                    );
+                  }
+
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
                 },
               ),
             ],
