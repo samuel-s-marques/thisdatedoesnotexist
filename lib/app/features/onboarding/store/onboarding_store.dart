4d9a0248ba748c3001c5d31185f66e5af706c592
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-22 17:07:54 -0300

    add hobbies api

--- lib/app/features/onboarding/store/onboarding_store.dart
+++ lib/app/features/onboarding/store/onboarding_store.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:mobx/mobx.dart';
@@ -8,7 +11,27 @@ part 'onboarding_store.g.dart';
 class OnboardingStore = OnboardingStoreBase with _$OnboardingStore;
 
 abstract class OnboardingStoreBase with Store {
+  String server = const String.fromEnvironment('SERVER');
+
   Future<List<Hobby>> getHobbies() async {
-    return [];
+    final Dio dio = Dio();
+    final List<Hobby> hobbies = [];
+
+    final Response response = await dio.get('$server/api/hobbies');
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = response.data['data'];
+
+      for (int index = 0; index < data.length; index++) {
+        final String name = data[index]['name'];
+        final String type = data[index]['type'];
+
+        hobbies.add(Hobby(name: name, type: type));
+      }
+
+      return hobbies;
+    } else {
+      return hobbies;
+    }
   }
 }
