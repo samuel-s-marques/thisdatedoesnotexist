8b0e03e029348ed5f172165fb621e2535e56afc3
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-23 22:53:22 -0300

    Refactor home store and add preferences functionality

--- lib/app/features/home/store/home_store.dart
+++ lib/app/features/home/store/home_store.dart
@@ -2,9 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:thisdatedoesnotexist/app/core/services/auth_service.dart';
 import 'package:thisdatedoesnotexist/app/features/chat/chat_module.dart';
 import 'package:thisdatedoesnotexist/app/features/home/models/character_model.dart';
@@ -17,7 +17,9 @@ class HomeStore = HomeStoreBase with _$HomeStore;
 abstract class HomeStoreBase with Store {
   FirebaseFirestore db = FirebaseFirestore.instance;
   AuthService authService = AuthService();
+  final Dio dio = Dio();
   String server = const String.fromEnvironment('SERVER');
+  SharedPreferences? prefs;
 
   @observable
   int swipes = 20;
@@ -30,32 +32,82 @@ abstract class HomeStoreBase with Store {
   @observable
   List<CharacterModel> cards = [];
 
-  Map<String, List<Widget>> appbars = {
-    'Home': [
-      IconButton(
-        onPressed: () {},
-        icon: const Icon(Icons.tune),
-      )
-    ],
-    'Chat': [
-      IconButton(
-        onPressed: () {},
-        icon: const Icon(Icons.search),
-      )
-    ],
-    'Profile': [
-      IconButton(
-        onPressed: () {},
-        icon: const Icon(Icons.edit),
-      ),
-      IconButton(
-        onPressed: () => Modular.to.pushNamed('/settings/'),
-        icon: const Icon(Icons.settings),
-      ),
-    ]
-  };
+  List<String> appbars = ['Home', 'Chat', 'Profile'];
 
   CardSwiperController cardSwiperController = CardSwiperController();
+
+  @observable
+  String selectedRelationshipGoalPreference = '';
+
+  @observable
+  String selectedPoliticalViewPreference = '';
+
+  @observable
+  RangeValues ageValues = const RangeValues(18, 50);
+
+  @observable
+  ObservableList<String> relationshipGoals = ObservableList();
+
+  @observable
+  ObservableList<String> politicalViews = ObservableList();
+
+  @action
+  Future<void> setAgeValues(RangeValues values) async {
+    ageValues = values;
+    await prefs!.setDouble('minAge', ageValues.start);
+    await prefs!.setDouble('maxAge', ageValues.end);
+  }
+
+  @action
+  Future<void> selectPoliticalViewPreference(String view) async {
+    selectedPoliticalViewPreference = view;
+    await prefs!.setString('politicalView', selectedPoliticalViewPreference);
+  }
+
+  @action
+  Future<void> selectRelationshipGoalPreference(String goal) async {
+    selectedRelationshipGoalPreference = goal;
+    await prefs!.setString('relationshipGoal', selectedRelationshipGoalPreference);
+  }
+
+  Future<void> getRelationshipGoals() async {
+    final Response response = await dio.get('$server/api/relationship-goals');
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = response.data['data'];
+
+      for (int index = 0; index < data.length; index++) {
+        final String goal = data[index]['name'];
+        relationshipGoals.add(goal);
+      }
+      relationshipGoals.add('All');
+    }
+  }
+
+  Future<void> getPoliticalViews() async {
+    final Response response = await dio.get('$server/api/political-views');
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = response.data['data'];
+
+      for (int index = 0; index < data.length; index++) {
+        final String view = data[index]['name'];
+        politicalViews.add(view);
+      }
+      politicalViews.add('All');
+    }
+  }
+
+  Future<void> getPreferences() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    ageValues = RangeValues(
+      prefs.getDouble('minAge') ?? 18,
+      prefs.getDouble('maxAge') ?? 50,
+    );
+    selectedPoliticalViewPreference = prefs.getString('politicalView') ?? 'All';
+    selectedRelationshipGoalPreference = prefs.getString('relationshipGoal') ?? 'All';
+  }
 
   @action
   Future<bool> onSwipe(
@@ -96,7 +148,7 @@ abstract class HomeStoreBase with Store {
   @action
   Future<List<CharacterModel>> getTodayCards() async {
     final Response<dynamic> response = await Dio().get(
-      '$server/api/character?sex=female&minAge=18&maxAge=21&user=${authService.getUser().uid}',
+      '$server/api/characters',
     );
 
     if (response.statusCode == 200) {
