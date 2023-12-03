da5fc45122abcc07ce5335a1fc2a5c700ef63d81
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-02 23:17:39 -0300

    Add import statements and update Preferences model

--- lib/app/core/models/preferences_model.dart
+++ lib/app/core/models/preferences_model.dart
@@ -1,3 +1,8 @@
+import 'package:thisdatedoesnotexist/app/core/models/body_type_model.dart';
+import 'package:thisdatedoesnotexist/app/core/models/political_view_model.dart';
+import 'package:thisdatedoesnotexist/app/core/models/relationship_goal_model.dart';
+import 'package:thisdatedoesnotexist/app/core/models/sex_model.dart';
+
 class Preferences {
   Preferences({
     required this.sexes,
@@ -9,11 +14,16 @@ class Preferences {
   });
 
   factory Preferences.fromMap(Map<String, dynamic> map) {
+    final List<dynamic> sexes = map['sexes'] ?? [];
+    final List<dynamic> relationshipGoals = map['relationship_goals'] ?? [];
+    final List<dynamic> politicalViews = map['political_views'] ?? [];
+    final List<dynamic> bodyTypes = map['body_types'] ?? [];
+
     return Preferences(
-      sexes: List<String>.from(map['sexes'] ?? []),
-      relationshipGoals: List<String>.from(map['relationship_goals'] ?? []),
-      politicalViews: List<String>.from(map['political_views'] ?? []),
-      bodyTypes: List<String>.from(map['body_types'] ?? []),
+      sexes: sexes.map((sex) => Sex.fromMap(sex)).toList(),
+      relationshipGoals: relationshipGoals.map((goal) => RelationshipGoal.fromMap(goal)).toList(),
+      politicalViews: politicalViews.map((view) => PoliticalView.fromMap(view)).toList(),
+      bodyTypes: bodyTypes.map((type) => BodyType.fromMap(type)).toList(),
       minAge: (map['min_age'] ?? 18) as double,
       maxAge: (map['max_age'] ?? 50) as double,
     );
@@ -21,19 +31,19 @@ class Preferences {
 
   Map<String, dynamic> toMap() {
     return {
-      'sexes': sexes,
-      'relationship_goals': relationshipGoals,
-      'political_views': politicalViews,
-      'body_types': bodyTypes,
+      'sexes': sexes.map((sex) => sex.toMap()).toList(),
+      'relationship_goals': relationshipGoals.map((goal) => goal.toMap()).toList(),
+      'political_views': politicalViews.map((view) => view.toMap()).toList(),
+      'body_types': bodyTypes.map((type) => type.toMap()).toList(),
       'min_age': minAge,
       'max_age': maxAge,
     };
   }
 
-  final List<String> sexes;
-  final List<String> relationshipGoals;
-  final List<String> politicalViews;
-  final List<String> bodyTypes;
+  final List<Sex> sexes;
+  final List<RelationshipGoal> relationshipGoals;
+  final List<PoliticalView> politicalViews;
+  final List<BodyType> bodyTypes;
   final double minAge;
   final double maxAge;
 }
