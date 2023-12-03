94765463960ff040e4b42a9dcf2bc835265f1aea
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-02 23:18:46 -0300

    Add user authentication and database service to profile store

--- lib/app/features/profile/store/profile_store.dart
+++ lib/app/features/profile/store/profile_store.dart
@@ -1,7 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mobx/mobx.dart';
+import 'package:thisdatedoesnotexist/app/core/models/user_model.dart';
+import 'package:thisdatedoesnotexist/app/core/services/auth_service.dart';
+import 'package:thisdatedoesnotexist/app/core/services/database_service.dart';
 
 part 'profile_store.g.dart';
 
 class ProfileStore = ProfileStoreBase with _$ProfileStore;
 
-abstract class ProfileStoreBase with Store {}
+abstract class ProfileStoreBase with Store {
+  String server = const String.fromEnvironment('SERVER');
+  DatabaseService databaseService = DatabaseService();
+  AuthService authService = AuthService();
+  User? authenticatedUser;
+  UserModel? user;
+  final Dio dio = Dio();
+
+  @action
+  Future<void> getUser() async {
+    authenticatedUser = authService.getUser();
+    user = await databaseService.getUser();
+  }
+}
