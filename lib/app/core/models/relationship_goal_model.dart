074b6e26ab64a16fbfd3cdac9dddee489af0e105
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-03 17:39:31 -0300

    Add equality methods to RelationshipGoal model

--- lib/app/core/models/relationship_goal_model.dart
+++ lib/app/core/models/relationship_goal_model.dart
@@ -18,6 +18,22 @@ class RelationshipGoal {
     };
   }
 
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+
+    return other is RelationshipGoal && other.id == id && other.name == name;
+  }
+
+  @override
+  int get hashCode => Object.hash(id, name);
+
   final int? id;
   final String? name;
 }
