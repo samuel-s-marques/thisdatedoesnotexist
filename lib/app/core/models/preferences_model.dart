ad04a7eb988e03de81c58fe9f5976051688df8d6
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-04 18:26:55 -0300

    Add Religion model to Preferences class

--- lib/app/core/models/preferences_model.dart
+++ lib/app/core/models/preferences_model.dart
@@ -1,6 +1,7 @@
 import 'package:thisdatedoesnotexist/app/core/models/body_type_model.dart';
 import 'package:thisdatedoesnotexist/app/core/models/political_view_model.dart';
 import 'package:thisdatedoesnotexist/app/core/models/relationship_goal_model.dart';
+import 'package:thisdatedoesnotexist/app/core/models/religion_model.dart';
 import 'package:thisdatedoesnotexist/app/core/models/sex_model.dart';
 import 'package:thisdatedoesnotexist/app/core/util.dart';
 
@@ -10,6 +11,7 @@ class Preferences {
     required this.relationshipGoals,
     required this.politicalViews,
     required this.bodyTypes,
+    required this.religions,
     required this.minAge,
     required this.maxAge,
   });
@@ -19,6 +21,7 @@ class Preferences {
     final List<dynamic> relationshipGoals = map['relationship_goals'] ?? [];
     final List<dynamic> politicalViews = map['political_views'] ?? [];
     final List<dynamic> bodyTypes = map['body_types'] ?? [];
+    final List<dynamic> religions = map['religions'] ?? [];
     final double minAge = checkDouble(map['min_age'] ?? 18);
     final double maxAge = checkDouble(map['max_age'] ?? 50);
 
@@ -27,6 +30,7 @@ class Preferences {
       relationshipGoals: relationshipGoals.map((goal) => RelationshipGoal.fromMap(goal)).toList(),
       politicalViews: politicalViews.map((view) => PoliticalView.fromMap(view)).toList(),
       bodyTypes: bodyTypes.map((type) => BodyType.fromMap(type)).toList(),
+      religions: religions.map((religion) => Religion.fromMap(religion)).toList(),
       minAge: minAge,
       maxAge: maxAge,
     );
@@ -38,6 +42,7 @@ class Preferences {
       'relationship_goals': relationshipGoals.map((goal) => goal.toMap()).toList(),
       'political_views': politicalViews.map((view) => view.toMap()).toList(),
       'body_types': bodyTypes.map((type) => type.toMap()).toList(),
+      'religions': religions.map((religion) => religion.toMap()).toList(),
       'min_age': minAge,
       'max_age': maxAge,
     };
@@ -47,6 +52,7 @@ class Preferences {
   final List<RelationshipGoal> relationshipGoals;
   final List<PoliticalView> politicalViews;
   final List<BodyType> bodyTypes;
+  final List<Religion> religions;
   final double minAge;
   final double maxAge;
 }
