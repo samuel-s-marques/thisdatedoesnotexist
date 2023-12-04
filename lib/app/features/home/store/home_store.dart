ed1a6bc7711fa1ed231ffdc233e0bdf14202e015
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-04 18:27:02 -0300

    Add Religion model and preferences to HomeStore

--- lib/app/features/home/store/home_store.dart
+++ lib/app/features/home/store/home_store.dart
@@ -9,6 +9,7 @@ import 'package:thisdatedoesnotexist/app/core/models/body_type_model.dart';
 import 'package:thisdatedoesnotexist/app/core/models/political_view_model.dart';
 import 'package:thisdatedoesnotexist/app/core/models/preferences_model.dart';
 import 'package:thisdatedoesnotexist/app/core/models/relationship_goal_model.dart';
+import 'package:thisdatedoesnotexist/app/core/models/religion_model.dart';
 import 'package:thisdatedoesnotexist/app/core/models/sex_model.dart';
 import 'package:thisdatedoesnotexist/app/core/services/auth_service.dart';
 import 'package:thisdatedoesnotexist/app/core/util.dart';
@@ -55,6 +56,9 @@ abstract class HomeStoreBase with Store {
   ObservableList<BodyType> bodyTypes = ObservableList();
 
   @observable
+  ObservableList<Religion> religions = ObservableList();
+
+  @observable
   ObservableList<Sex> sexes = ObservableList();
   Map<String, String> sexesMap = {'male': 'Men', 'female': 'Women'};
 
@@ -68,6 +72,9 @@ abstract class HomeStoreBase with Store {
   ObservableList<RelationshipGoal> selectedRelationshipGoalPreferences = ObservableList();
 
   @observable
+  ObservableList<Religion> selectedReligionPreferences = ObservableList();
+
+  @observable
   ObservableList<Sex> selectedSexPreferences = ObservableList();
 
   @action
@@ -79,6 +86,18 @@ abstract class HomeStoreBase with Store {
       selectedPoliticalViewPreferences.add(view);
     } else {
       selectedPoliticalViewPreferences.remove(view);
+    }
+  }
+
+  @action
+  Future<void> selectReligionPreference({
+    required bool selected,
+    required Religion religion,
+  }) async {
+    if (selected) {
+      selectedReligionPreferences.add(religion);
+    } else {
+      selectedReligionPreferences.remove(religion);
     }
   }
 
@@ -134,6 +153,7 @@ abstract class HomeStoreBase with Store {
       final List<dynamic> relationshipGoals = data['relationship_goals'] ?? [];
       final List<dynamic> politicalViews = data['political_views'] ?? [];
       final List<dynamic> bodyTypes = data['body_types'] ?? [];
+      final List<dynamic> religions = data['religions'] ?? [];
       final double minAge = checkDouble(data['min_age'] ?? 18);
       final double maxAge = checkDouble(data['max_age'] ?? 50);
 
@@ -141,6 +161,7 @@ abstract class HomeStoreBase with Store {
       selectedBodyTypePreferences = ObservableList.of(bodyTypes.map((type) => BodyType.fromMap(type)).toList());
       selectedPoliticalViewPreferences = ObservableList.of(politicalViews.map((view) => PoliticalView.fromMap(view)).toList());
       selectedSexPreferences = ObservableList.of(sexes.map((sex) => Sex.fromMap(sex)).toList());
+      selectedReligionPreferences = ObservableList.of(religions.map((religion) => Religion.fromMap(religion)).toList());
       ageValues = RangeValues(minAge, maxAge);
 
       return true;
@@ -197,6 +218,19 @@ abstract class HomeStoreBase with Store {
       for (int index = 0; index < data.length; index++) {
         final BodyType bodyType = BodyType.fromMap(data[index]);
         bodyTypes.add(bodyType);
+      }
+    }
+  }
+
+  Future<void> getReligions() async {
+    final Response response = await dio.get('$server/api/religions');
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = response.data['data'];
+
+      for (int index = 0; index < data.length; index++) {
+        final Religion religion = Religion.fromMap(data[index]);
+        religions.add(religion);
       }
     }
   }
@@ -283,6 +317,7 @@ abstract class HomeStoreBase with Store {
       relationshipGoals: selectedRelationshipGoalPreferences,
       politicalViews: selectedPoliticalViewPreferences,
       bodyTypes: selectedBodyTypePreferences,
+      religions: selectedReligionPreferences,
       minAge: ageValues.start,
       maxAge: ageValues.end,
     );
@@ -302,7 +337,7 @@ abstract class HomeStoreBase with Store {
   @action
   Future<bool?> getTodayCards() async {
     if (await getPreferences()) {
-      String url = '$server/api/characters?min_age=${ageValues.start.round()}&max_age=${ageValues.end.round()}';
+      String url = '$server/api/characters?uid=${authenticatedUser?.uid}min_age=${ageValues.start.round()}&max_age=${ageValues.end.round()}';
 
       if (selectedPoliticalViewPreferences.isNotEmpty) {
         final List<String?> politicalViews = selectedPoliticalViewPreferences.map((e) => e.name).toList();
@@ -322,6 +357,11 @@ abstract class HomeStoreBase with Store {
       if (selectedSexPreferences.isNotEmpty) {
         final List<String?> sexes = selectedSexPreferences.map((e) => e.name).toList();
         url += '&sex=${sexes.join(',')}';
+      }
+
+      if (selectedReligionPreferences.isNotEmpty) {
+        final List<String?> religions = selectedReligionPreferences.map((e) => e.name).toList();
+        url += '&religion=${religions.join(',')}';
       }
 
       final Response<dynamic> response = await Dio().get(url);
