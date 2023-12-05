e93da1a11fb89abb2e46a6cafffda052d8c76528
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-05 16:49:40 -0300

    Refactor code to use arrow functions

--- lib/app/features/auth/store/auth_store.dart
+++ lib/app/features/auth/store/auth_store.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
