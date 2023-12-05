fb847e9d7cf18f5807e0877a6ddbbf5ee8c9042d
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-05 16:49:58 -0300

    Add Sentry Flutter package import

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:feedback/feedback.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:thisdatedoesnotexist/app/app_module.dart';
 import 'package:thisdatedoesnotexist/app/app_widget.dart';
 import 'package:thisdatedoesnotexist/firebase_options.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
