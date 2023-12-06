887cacdea8532ad7018ceccd140415227a375f7e
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-06 15:02:15 -0300

    Add SearchableListView widget

---
+++ lib/app/core/widgets/searchable_list_view.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:thisdatedoesnotexist/app/core/models/base_model.dart';
+
+class SearchableListView extends StatefulWidget {
+  const SearchableListView({
+    super.key,
+    required this.items,
+    required this.onSearch,
+    required this.onSelectedItem,
+  });
+
+  final List<BaseModel> items;
+  final Function(String) onSearch;
+  final Function(BaseModel) onSelectedItem;
+
+  @override
+  State<SearchableListView> createState() => _SearchableListViewState();
+}
+
+class _SearchableListViewState extends State<SearchableListView> {
+  final TextEditingController searchController = TextEditingController();
+  List<BaseModel> filteredItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    filteredItems = widget.items;
+    searchController.addListener(() {
+      final searchTerm = searchController.text.toLowerCase();
+      widget.onSearch(searchTerm);
+
+      setState(() {
+        filteredItems = widget.items.where((item) {
+          final itemName = item.name!.toLowerCase();
+          return itemName.contains(searchTerm);
+        }).toList();
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          controller: searchController,
+          decoration: const InputDecoration(
+            hintText: 'Search',
+            prefix: Icon(Icons.search),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: filteredItems.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(filteredItems[index].name!),
+                onTap: () => widget.onSelectedItem(filteredItems[index]),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
