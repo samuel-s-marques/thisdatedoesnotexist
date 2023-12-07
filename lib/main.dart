49053f63261e9da5b828e4429e0f9236596712d9
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-07 14:49:03 -0300

    Add OneSignal initialization and permission request

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,19 @@ import 'package:feedback/feedback.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:thisdatedoesnotexist/app/app_module.dart';
 import 'package:thisdatedoesnotexist/app/app_widget.dart';
 import 'package:thisdatedoesnotexist/firebase_options.dart';
 
 void main() async {
+  const String oneSignalAppId = String.fromEnvironment('ONESIGNAL_APP_ID');
   WidgetsFlutterBinding.ensureInitialized();
+
+  OneSignal.initialize(oneSignalAppId);
+  await OneSignal.Notifications.requestPermission(true);
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await SentryFlutter.init(
     (options) {
