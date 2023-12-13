46aad52f2a52d588627021d47005a286b4631e91
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-12 23:56:01 -0300

    Remove unnecessary method call in AuthPageState

--- lib/app/features/auth/pages/auth_page.dart
+++ lib/app/features/auth/pages/auth_page.dart
@@ -20,7 +20,6 @@ class _AuthPageState extends State<AuthPage> {
     store.emailController = TextEditingController();
     store.passwordController = TextEditingController();
     store.repeatPasswordController = TextEditingController();
-    store.checkLogin();
 
     super.initState();
   }
