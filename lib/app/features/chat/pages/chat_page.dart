2baa835746202e7d6704defb46e6e73bf8b3f4ac
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-18 01:40:14 -0300

    Add character avatar and name to chat page

--- lib/app/features/chat/pages/chat_page.dart
+++ lib/app/features/chat/pages/chat_page.dart
@@ -1,40 +1,63 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_firebase_chat_core/flutter_firebase_chat_core.dart';
+import 'package:thisdatedoesnotexist/app/core/models/character_model.dart';
+import 'package:thisdatedoesnotexist/app/features/chat/store/chat_store.dart';
 
 class ChatPage extends StatefulWidget {
-  const ChatPage({super.key});
+  const ChatPage({
+    super.key,
+    required this.id,
+  });
+
+  final String id;
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
+  ChatStore store = ChatStore();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Chat name'),
-      ),
-      body: StreamBuilder<List<types.Message>>(
-        initialData: const [],
-        stream: FirebaseChatCore.instance.messages(
-          const types.Room(
-            id: 'id',
-            type: types.RoomType.direct,
-            users: [],
-          ),
-        ),
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          return Chat(
-            messages: const [],
-            onSendPressed: (types.PartialText message) {},
-            user: const types.User(id: '0'),
+    return FutureBuilder(
+      future: store.getCharacterById(widget.id),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.hasData) {
+          final CharacterModel character = CharacterModel.fromMap(snapshot.data);
+
+          return Scaffold(
+            appBar: AppBar(
+              leadingWidth: 30,
+              title: Row(
+                children: [
+                  CircleAvatar(
+                    maxRadius: 18,
+                    backgroundImage: CachedNetworkImageProvider(
+                      '${store.server}/uploads/characters/${character.uid}.png',
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Text('${character.name} ${character.surname}'),
+                ],
+              ),
+            ),
+            body: Chat(
+              messages: const [],
+              onSendPressed: (types.PartialText message) {},
+              user: const types.User(id: '0'),
+            ),
           );
-        },
-      ),
+        }
+
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 }
