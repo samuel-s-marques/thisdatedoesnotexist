be2f0547348a304cb951ef738ed600810d0b35f6
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-18 00:50:49 -0300

    Add character model and chat store to chat list page

--- lib/app/features/chat/pages/chat_list_page.dart
+++ lib/app/features/chat/pages/chat_list_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:thisdatedoesnotexist/app/core/models/character_model.dart';
+import 'package:thisdatedoesnotexist/app/features/chat/store/chat_store.dart';
 import 'package:thisdatedoesnotexist/app/features/chat/widgets/chat_list_tile.dart';
 
 class ChatListPage extends StatefulWidget {
@@ -9,18 +11,35 @@ class ChatListPage extends StatefulWidget {
 }
 
 class _ChatListPageState extends State<ChatListPage> {
+  ChatStore store = ChatStore();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 0,
-        itemBuilder: (BuildContext context, int index) {
-          return ChatListTile(
-            id: '0',
-            name: 'Samuel',
-            message: 'Hiiii',
-            time: DateTime.now(),
-            avatarUrl: 'https://placehold.co/256x256/png',
+      body: FutureBuilder(
+        future: store.getChats(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasData) {
+            final List<dynamic> data = snapshot.data['data'];
+
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (BuildContext context, int index) {
+                final CharacterModel character = CharacterModel.fromMap(data[index]['character']);
+
+                return ChatListTile(
+                  id: character.uid,
+                  name: '${character.name} ${character.surname}',
+                  message: 'Hiiii',
+                  time: character.updatedAt,
+                  avatarUrl: '${store.server}/uploads/characters/${character.uid}.png',
+                );
+              },
+            );
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(),
           );
         },
       ),
