912e399961f1b713697582daad74e89a6eee6e9b
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-18 00:50:52 -0300

    Add getChats() method to ChatStore

--- lib/app/features/chat/store/chat_store.dart
+++ lib/app/features/chat/store/chat_store.dart
@@ -1,7 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mobx/mobx.dart';
+import 'package:thisdatedoesnotexist/app/core/services/auth_service.dart';
+import 'package:thisdatedoesnotexist/app/features/chat/widgets/chat_list_tile.dart';
 
 part 'chat_store.g.dart';
 
 class ChatStore = ChatStoreBase with _$ChatStore;
 
-abstract class ChatStoreBase with Store {}
+abstract class ChatStoreBase with Store {
+  AuthService authService = AuthService();
+  User? authenticatedUser;
+  final Dio dio = Dio();
+  String server = const String.fromEnvironment('SERVER');
+
+  @action
+  Future<dynamic> getChats() async {
+    authenticatedUser ??= authService.getUser();
+
+    final Response<dynamic> response = await dio.get('$server/api/chats?uid=${authenticatedUser?.uid}');
+    
+    return response.data;
+  }
+}
