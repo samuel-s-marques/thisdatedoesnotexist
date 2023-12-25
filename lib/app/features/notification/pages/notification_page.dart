eb58caedfa15f9903b5f40e383c37d95ffe7697b
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-25 17:10:27 -0300

    Add app bar to NotificationPage

--- lib/app/features/notification/pages/notification_page.dart
+++ lib/app/features/notification/pages/notification_page.dart
@@ -12,6 +12,9 @@ class NotificationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notifications'),
+      ),
       body: FutureBuilder(
         future: store.getNotifications(),
         builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
