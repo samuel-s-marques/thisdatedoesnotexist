911130c2e61891b04e586f9949f16527b23d8dc9
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-26 15:33:41 -0300

    Add notification listener in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:feedback/feedback.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:hive/hive.dart';
@@ -10,6 +11,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:thisdatedoesnotexist/app/app_module.dart';
 import 'package:thisdatedoesnotexist/app/app_widget.dart';
+import 'package:thisdatedoesnotexist/app/features/notification/store/notification_store.dart';
 import 'package:thisdatedoesnotexist/firebase_options.dart';
 
 void main() async {
@@ -27,6 +29,20 @@ void main() async {
       final Directory hiveDir = await getTemporaryDirectory();
       Hive.init(hiveDir.path);
       await Hive.openBox('thisdatedoesnotexist');
+
+      OneSignal.Notifications.addForegroundWillDisplayListener((notificationReceivedEvent) {
+        try {
+          final NotificationStore? notificationStore = Modular.tryGet<NotificationStore>();
+
+          if (notificationStore != null) {
+            notificationStore.setNotificationState(value: true);
+          }
+        } catch (e) {
+          if (kDebugMode) {
+            print(e);
+          }
+        }
+      });
 
       return runApp(
         BetterFeedback(
