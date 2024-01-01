616e6c8c3339cba814dceb198716f4e6375004fb
Samuel Marques <samuel.s.marques@protonmail.com>
2024-01-01 17:41:57 -0300

    Add SecurityPage to Settings feature

---
+++ lib/app/features/settings/pages/security_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:thisdatedoesnotexist/app/features/settings/store/settings_store.dart';
+import 'package:thisdatedoesnotexist/app/features/settings/widgets/settings_tile.dart';
+
+class SecurityPage extends StatelessWidget {
+  SecurityPage({super.key});
+
+  final SettingsStore store = SettingsStore();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Help & Support'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 15),
+        children: [
+          SettingsTile(
+            title: 'Change Password',
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {},
+          ),
+          SettingsTile(
+            title: 'Change Email',
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {},
+          ),
+          SettingsTile(
+            title: 'Blocked Characters',
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {},
+          ),
+          SettingsTile(
+            title: 'Reported Characters',
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
