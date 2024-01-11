ea0dc41a90ee4543637e7bab1f62247014da5a75
Samuel Marques <samuel.s.marques@protonmail.com>
2024-01-11 17:57:33 -0300

    Add imports and implement WebSocket functionality for chat feature

--- lib/app/features/chat/store/chat_store.dart
+++ lib/app/features/chat/store/chat_store.dart
@@ -1,13 +1,18 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:mobx/mobx.dart';
 import 'package:thisdatedoesnotexist/app/core/models/user_model.dart';
 import 'package:thisdatedoesnotexist/app/core/services/auth_service.dart';
 import 'package:thisdatedoesnotexist/app/core/services/dio_service.dart';
 import 'package:thisdatedoesnotexist/app/core/util.dart';
+import 'package:thisdatedoesnotexist/app/features/chat/models/chat_model.dart';
 import 'package:uuid/uuid.dart';
-import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 part 'chat_store.g.dart';
 
@@ -20,23 +25,101 @@ abstract class ChatStoreBase with Store {
   String server = const String.fromEnvironment('SERVER');
   String wssServer = const String.fromEnvironment('WSS_SERVER');
   final DioService dio = DioService();
+  WebSocketChannel? channel;
+  BuildContext? buildContext;
+  Timer? timer;
+  bool requestedChats = false;
+  bool firstRequest = false;
 
   @observable
   UserModel? character;
 
-  @action
-  Future<dynamic> getChats() async {
+  @observable
+  ObservableList<ChatModel> chats = ObservableList();
+
+  Future<void> authenticateUser() async {
     authenticatedUser ??= authService.getUser();
 
-    final Response<dynamic> response = await dio.get('$server/api/chats?uid=${authenticatedUser?.uid}', options: DioOptions(cache: false));
+    channel!.sink.add(jsonEncode({
+      'type': 'auth',
+      'user_id': authenticatedUser!.uid,
+      'token': await authenticatedUser!.getIdToken(),
+    }));
+  }
 
-    return response.data;
+  @action
+  Future<void> handleWebSocketMessage(dynamic data) async {
+    if (authenticatedUser == null) {
+      await authenticateUser();
+    }
+
+    if (authenticatedUser != null) {
+      if (!firstRequest) {
+        channel!.sink.add(
+          jsonEncode({
+            'type': 'chats',
+          }),
+        );
+        firstRequest = true;
+      }
+
+      if (!requestedChats) {
+        timer?.cancel();
+        timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+          channel!.sink.add(
+            jsonEncode({
+              'type': 'chats',
+            }),
+          );
+          requestedChats = true;
+        });
+      }
+
+      if (data != null) {
+        final Map<String, dynamic> json = jsonDecode(data);
+
+        if (json['type'] == 'system') {
+          switch (json['status']) {
+            case 'error':
+              buildContext!.showSnackBarError(message: json['message']);
+              break;
+            case 'success':
+              buildContext!.showSnackBarSuccess(message: json['message']);
+              break;
+            default:
+              buildContext!.showSnackBar(message: json['message']);
+          }
+        }
+
+        if (json['type'] == 'chats') {
+          final List<ChatModel> tempChats = [];
+
+          for (int index = 0; index < json['data']['data'].length; index++) {
+            final Map<String, dynamic> data = json['data']['data'][index];
+
+            final UserModel character = UserModel.fromMap(data['character']);
+            final ChatModel chat = ChatModel(
+              uid: character.uid,
+              avatarUrl: '$server/uploads/characters/${character.uid}.png',
+              name: '${character.name} ${character.surname}',
+              lastMessage: data['last_message'],
+              seen: data['seen'] != 0,
+              updatedAt: DateTime.parse(data['updated_at']),
+            );
+
+            tempChats.add(chat);
+          }
+
+          chats.clear();
+          chats.addAll(tempChats);
+          requestedChats = false;
+        }
+      }
+    }
   }
 
   @action
   Future<UserModel?> getCharacterById(String id) async {
-    authenticatedUser ??= authService.getUser();
-
     final Response<dynamic> response = await dio.get('$server/api/characters/$id', options: DioOptions());
 
     if (response.statusCode == 200) {
@@ -48,7 +131,6 @@ abstract class ChatStoreBase with Store {
 
   @action
   Future<List<types.Message>> getMessages(String id, int page, void Function(int) updateAvailablePages) async {
-    authenticatedUser ??= authService.getUser();
     final List<types.Message> messages = [];
     final Response<dynamic> response = await dio.get('$server/api/messages?uid=$id&page=$page', options: DioOptions(cache: false));
 
