ed4d803293bce5fde19fb1dda5d70324719b55ff
Samuel Marques <samuel.s.marques@protonmail.com>
2024-01-11 17:56:02 -0300

    Add uid and avatarUrl properties to ChatModel

--- lib/app/features/chat/models/chat_model.dart
+++ lib/app/features/chat/models/chat_model.dart
@@ -1,5 +1,7 @@
 class ChatModel {
   ChatModel({
+    required this.uid,
+    required this.avatarUrl,
     required this.name,
     this.lastMessage,
     this.seen = false,
@@ -8,6 +10,8 @@ class ChatModel {
 
   factory ChatModel.fromMap(Map<dynamic, dynamic> map) {
     return ChatModel(
+      uid: map['uid'],
+      avatarUrl: map['avatar_url'],
       name: map['name'],
       lastMessage: map['last_message'],
       seen: map['seen'],
@@ -15,6 +19,8 @@ class ChatModel {
     );
   }
 
+  final String uid;
+  final String avatarUrl;
   final String name;
   final String? lastMessage;
   final bool seen;
@@ -27,6 +33,8 @@ class ChatModel {
     DateTime? updatedAt,
   }) {
     return ChatModel(
+      uid: uid,
+      avatarUrl: avatarUrl,
       name: name ?? this.name,
       lastMessage: lastMessage ?? this.lastMessage,
       seen: seen ?? this.seen,
@@ -36,6 +44,8 @@ class ChatModel {
 
   Map<String, dynamic> toMap() {
     return {
+      'uid': uid,
+      'avatar_url': avatarUrl,
       'name': name,
       'last_message': lastMessage,
       'seen': seen,
