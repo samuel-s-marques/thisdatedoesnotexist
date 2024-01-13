e039315bbdea0edaf97af6c6c1047289275fcb58
Samuel Marques <samuel.s.marques@protonmail.com>
2024-01-13 20:06:42 -0300

    Add ChatBubble widget for displaying chat messages

---
+++ lib/app/features/chat/widgets/chat_bubble.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:thisdatedoesnotexist/app/features/chat/widgets/message_type_enum.dart';
+
+class ChatBubble extends StatelessWidget {
+  const ChatBubble({
+    super.key,
+    required this.type,
+    required this.message,
+  });
+
+  final MessageType type;
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    final Map<MessageType, Map<String, dynamic>> details = {
+      MessageType.system: {
+        'alignment': MainAxisAlignment.center,
+        'color': Colors.grey,
+      },
+      MessageType.sender: {
+        'alignment': MainAxisAlignment.start,
+        'color': Theme.of(context).primaryColor,
+      },
+      MessageType.user: {
+        'alignment': MainAxisAlignment.end,
+        'color': Theme.of(context).secondaryHeaderColor,
+      },
+    };
+
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        mainAxisAlignment: details[type]!['alignment']!,
+        children: [
+          Container(
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width * 0.7,
+            ),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: details[type]!['color']!,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text(
+              message,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
