34c08caa6ce6fbce31431ca8f155d528c6a0db0c
Samuel Marques <samuel.s.marques@protonmail.com>
2024-01-14 18:17:30 -0300

    Add draft support to ChatListTile widget

--- lib/app/features/chat/widgets/chat_list_tile.dart
+++ lib/app/features/chat/widgets/chat_list_tile.dart
@@ -9,12 +9,15 @@ class ChatListTile extends StatelessWidget {
     required this.id,
     required this.name,
     this.message,
+    this.draft,
     required this.time,
     required this.avatarUrl,
   });
+
   final String id;
   final String name;
   final String? message;
+  final String? draft;
   final DateTime time;
   final String avatarUrl;
 
@@ -22,9 +25,31 @@ class ChatListTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(name),
-      subtitle: message != null
-          ? Text(
-              message!,
+      subtitle: (message != null || draft != null)
+          ? Text.rich(
+              TextSpan(
+                children: [
+                  if (draft != null && draft!.isNotEmpty)
+                    TextSpan(
+                      text: 'Draft: ',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: draft,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.normal,
+                            color: Colors.black,
+                          )
+                        ),
+                      ],
+                    )
+                  else
+                    TextSpan(text: message)
+                ],
+              ),
               overflow: TextOverflow.ellipsis,
               maxLines: 1,
             )
