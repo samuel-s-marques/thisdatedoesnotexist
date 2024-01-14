b67dfd25281633e3dff51eafbaf3cc873cddfec5
Samuel Marques <samuel.s.marques@protonmail.com>
2024-01-14 17:01:09 -0300

    Refactor chat bubble widget to improve user interaction

--- lib/app/features/chat/widgets/chat_bubble.dart
+++ lib/app/features/chat/widgets/chat_bubble.dart
@@ -42,115 +42,114 @@ class ChatBubble extends StatelessWidget {
       },
     };
 
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(10),
-        onTap: () {
-          if (type == MessageType.system) {
-            return;
-          }
+    return IgnorePointer(
+      ignoring: type == MessageType.system,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(10),
+          onTap: () {
+            final RenderBox renderBox = context.findRenderObject() as RenderBox;
+            final Offset offset = renderBox.localToGlobal(Offset.zero);
+            final Size size = renderBox.size;
 
-          final RenderBox renderBox = context.findRenderObject() as RenderBox;
-          final Offset offset = renderBox.localToGlobal(Offset.zero);
-          final Size size = renderBox.size;
+            final double y = offset.dy;
+            final double x = offset.dx;
 
-          final double y = offset.dy;
-          final double x = offset.dx;
+            final double width = size.width;
+            final double height = size.height;
 
-          final double width = size.width;
-          final double height = size.height;
+            double left = width / 2;
+            double right = width / 2;
 
-          double left = width / 2;
-          double right = width / 2;
+            if (type == MessageType.sender) {
+              right = x;
+            } else {
+              left = x;
+            }
 
-          if (type == MessageType.sender) {
-            right = x;
-          } else {
-            left = x;
-          }
-
-          showMenu(
-            context: context,
-            position: RelativeRect.fromLTRB(left, y + height, right, 0),
-            items: [
-              PopupMenuItem(
-                padding: EdgeInsets.zero,
-                child: ListTile(
-                  leading: const Icon(Icons.copy_outlined),
-                  title: const Text('Copy'),
-                  onTap: () {
-                    Clipboard.setData(ClipboardData(text: message));
-                    Navigator.pop(context);
-                  },
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+            showMenu(
+              context: context,
+              position: RelativeRect.fromLTRB(left, y + height, right, 0),
+              items: [
+                PopupMenuItem(
+                  padding: EdgeInsets.zero,
+                  child: ListTile(
+                    leading: const Icon(Icons.copy_outlined),
+                    title: const Text('Copy'),
+                    onTap: () {
+                      Clipboard.setData(ClipboardData(text: message));
+                      Navigator.pop(context);
+                    },
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+                  ),
                 ),
-              ),
-              PopupMenuItem(
-                padding: EdgeInsets.zero,
-                child: ListTile(
-                  leading: const Icon(Icons.report_outlined),
-                  title: const Text('Report'),
-                  onTap: () {
-                    Navigator.pop(context);
-                    // TODO: Implement report
-                  },
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+                PopupMenuItem(
+                  padding: EdgeInsets.zero,
+                  child: ListTile(
+                    leading: const Icon(Icons.report_outlined),
+                    title: const Text('Report'),
+                    onTap: () {
+                      Navigator.pop(context);
+                      // TODO: Implement report
+                    },
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+                  ),
+                ),
+              ],
+            );
+          },
+          child: Row(
+            mainAxisAlignment: details[type]!['alignment']!,
+            children: [
+              Container(
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width * 0.7,
+                ),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: details[type]!['bubbleColor']!,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Theme(
+                      data: ThemeData(
+                        textSelectionTheme: TextSelectionThemeData(
+                          cursorColor: details[type]!['selectionColor']!,
+                          selectionColor: details[type]!['selectionColor']!,
+                          selectionHandleColor: details[type]!['selectionColor']!,
+                        ),
+                      ),
+                      child: SelectableText(
+                        message,
+                        style: TextStyle(
+                          color: details[type]!['textColor']!,
+                        ),
+                      ),
+                    ),
+                    if (type != MessageType.system)
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const SizedBox(height: 5),
+                          Text(
+                            DateFormat.Hm().format(createdAt!),
+                            style: TextStyle(
+                              color: details[type]!['textColor']!.withOpacity(0.5),
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+                  ],
                 ),
               ),
             ],
-          );
-        },
-        child: Row(
-          mainAxisAlignment: details[type]!['alignment']!,
-          children: [
-            Container(
-              constraints: BoxConstraints(
-                maxWidth: MediaQuery.of(context).size.width * 0.7,
-              ),
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: details[type]!['bubbleColor']!,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Theme(
-                    data: ThemeData(
-                      textSelectionTheme: TextSelectionThemeData(
-                        cursorColor: details[type]!['selectionColor']!,
-                        selectionColor: details[type]!['selectionColor']!,
-                        selectionHandleColor: details[type]!['selectionColor']!,
-                      ),
-                    ),
-                    child: SelectableText(
-                      message,
-                      style: TextStyle(
-                        color: details[type]!['textColor']!,
-                      ),
-                    ),
-                  ),
-                  if (type != MessageType.system)
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const SizedBox(height: 5),
-                        Text(
-                          DateFormat.Hm().format(createdAt!),
-                          style: TextStyle(
-                            color: details[type]!['textColor']!.withOpacity(0.5),
-                            fontSize: 12,
-                          ),
-                        ),
-                      ],
-                    ),
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
