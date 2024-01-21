9274ad89dd8058a3b23d1164424b70fd57a366a1
Samuel Marques <samuel.s.marques@protonmail.com>
2024-01-21 15:00:53 -0300

    Add linkify and url_launcher packages

--- lib/app/features/chat/widgets/chat_bubble.dart
+++ lib/app/features/chat/widgets/chat_bubble.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
 import 'package:intl/intl.dart';
 import 'package:thisdatedoesnotexist/app/core/util.dart';
 import 'package:thisdatedoesnotexist/app/features/chat/widgets/message_status_enum.dart';
 import 'package:thisdatedoesnotexist/app/features/chat/widgets/message_type_enum.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ChatBubble extends StatelessWidget {
   const ChatBubble({
@@ -14,6 +16,7 @@ class ChatBubble extends StatelessWidget {
     this.status,
     this.bubbleColor,
     this.textColor,
+    this.linkColor,
   });
 
   final MessageType type;
@@ -22,6 +25,7 @@ class ChatBubble extends StatelessWidget {
   final MessageStatus? status;
   final Color? bubbleColor;
   final Color? textColor;
+  final Color? linkColor;
 
   @override
   Widget build(BuildContext context) {
@@ -30,17 +34,20 @@ class ChatBubble extends StatelessWidget {
         'alignment': MainAxisAlignment.center,
         'bubbleColor': bubbleColor ?? Colors.grey,
         'textColor': textColor ?? Colors.white,
+        'linkColor': linkColor ?? Colors.blue,
         'selectionColor': Theme.of(context).primaryColorLight,
       },
       MessageType.sender: {
         'alignment': MainAxisAlignment.start,
         'bubbleColor': bubbleColor ?? Colors.white,
+        'linkColor': linkColor ?? Colors.blue,
         'textColor': textColor ?? Colors.black,
         'selectionColor': Theme.of(context).primaryColorLight,
       },
       MessageType.user: {
         'alignment': MainAxisAlignment.end,
         'bubbleColor': bubbleColor ?? Colors.deepPurple,
+        'linkColor': linkColor ?? const Color(0xFFC3F73A),
         'textColor': textColor ?? Colors.white,
         'selectionColor': Colors.deepPurple[200],
       },
@@ -115,10 +122,20 @@ class ChatBubble extends StatelessWidget {
                           selectionHandleColor: details[type]!['selectionColor']!,
                         ),
                       ),
-                      child: SelectableText(
-                        message,
+                      child: SelectableLinkify(
+                        text: message,
+                        onOpen: (LinkableElement link) async {
+                          if (await canLaunchUrl(Uri.parse(link.url))) {
+                            await launchUrl(Uri.parse(link.url));
+                          }
+                        },
                         style: TextStyle(
                           color: details[type]!['textColor']!,
+                        ),
+                        linkStyle: TextStyle(
+                          color: details[type]!['linkColor']!,
+                          decoration: TextDecoration.underline,
+                          decorationColor: details[type]!['linkColor']!,
                         ),
                       ),
                     ),
@@ -139,17 +156,17 @@ class ChatBubble extends StatelessWidget {
                                 ),
                               ),
                               if (type == MessageType.user)
-                              Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  const SizedBox(width: 5),
-                                  Icon(
-                                    status!.toIconData(),
-                                    size: 14,
-                                    color: details[type]!['textColor']!.withOpacity(0.5),
-                                  ),
-                                ],
-                              )
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    const SizedBox(width: 5),
+                                    Icon(
+                                      status!.toIconData(),
+                                      size: 14,
+                                      color: details[type]!['textColor']!.withOpacity(0.5),
+                                    ),
+                                  ],
+                                )
                             ],
                           )
                         ],
