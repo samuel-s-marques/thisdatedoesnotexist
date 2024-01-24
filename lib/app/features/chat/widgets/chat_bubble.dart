0998a6eaa4423d95e821c90fc1aa34862a0f1d45
Samuel Marques <samuel.s.marques@protonmail.com>
2024-01-24 15:07:24 -0300

    Refactor ChatBubble widget to use MessageFrom enum

--- lib/app/features/chat/widgets/chat_bubble.dart
+++ lib/app/features/chat/widgets/chat_bubble.dart
@@ -3,14 +3,14 @@ import 'package:flutter/services.dart';
 import 'package:flutter_emoji/flutter_emoji.dart';
 import 'package:thisdatedoesnotexist/app/core/util.dart';
 import 'package:thisdatedoesnotexist/app/features/chat/widgets/emoji_message_bubble.dart';
+import 'package:thisdatedoesnotexist/app/features/chat/widgets/message_from_enum.dart';
 import 'package:thisdatedoesnotexist/app/features/chat/widgets/message_status_enum.dart';
-import 'package:thisdatedoesnotexist/app/features/chat/widgets/message_type_enum.dart';
 import 'package:thisdatedoesnotexist/app/features/chat/widgets/text_message_bubble.dart';
 
 class ChatBubble extends StatelessWidget {
   const ChatBubble({
     super.key,
-    required this.type,
+    required this.from,
     required this.message,
     required this.createdAt,
     this.status,
@@ -19,7 +19,7 @@ class ChatBubble extends StatelessWidget {
     this.linkColor,
   });
 
-  final MessageType type;
+  final MessageFrom from;
   final String message;
   final DateTime createdAt;
   final MessageStatus? status;
@@ -29,22 +29,22 @@ class ChatBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Map<MessageType, Map<String, dynamic>> details = {
-      MessageType.system: {
+    final Map<MessageFrom, Map<String, dynamic>> details = {
+      MessageFrom.system: {
         'alignment': MainAxisAlignment.center,
         'bubbleColor': bubbleColor ?? Colors.grey,
         'textColor': textColor ?? Colors.white,
         'linkColor': linkColor ?? Colors.blue,
         'selectionColor': Theme.of(context).primaryColorLight,
       },
-      MessageType.sender: {
+      MessageFrom.sender: {
         'alignment': MainAxisAlignment.start,
         'bubbleColor': bubbleColor ?? Colors.white,
         'linkColor': linkColor ?? Colors.blue,
         'textColor': textColor ?? Colors.black,
         'selectionColor': Theme.of(context).primaryColorLight,
       },
-      MessageType.user: {
+      MessageFrom.user: {
         'alignment': MainAxisAlignment.end,
         'bubbleColor': bubbleColor ?? Colors.deepPurple,
         'linkColor': linkColor ?? const Color(0xFF28E2FB),
@@ -54,7 +54,7 @@ class ChatBubble extends StatelessWidget {
     };
 
     return IgnorePointer(
-      ignoring: type == MessageType.system,
+      ignoring: from == MessageFrom.system,
       child: Container(
         margin: const EdgeInsets.symmetric(vertical: 10),
         child: InkWell(
@@ -73,7 +73,7 @@ class ChatBubble extends StatelessWidget {
             double left = width / 2;
             double right = width / 2;
 
-            if (type == MessageType.sender) {
+            if (from == MessageFrom.sender) {
               right = x;
             } else {
               left = x;
@@ -99,13 +99,13 @@ class ChatBubble extends StatelessWidget {
             );
           },
           child: Row(
-            mainAxisAlignment: details[type]!['alignment']!,
+            mainAxisAlignment: details[from]!['alignment']!,
             children: [
               Builder(builder: (BuildContext context) {
-                final Color bubbleColor = details[type]!['bubbleColor']!;
-                final Color selectionColor = details[type]!['selectionColor']!;
-                final Color textColor = details[type]!['textColor']!;
-                final Color linkColor = details[type]!['linkColor']!;
+                final Color bubbleColor = details[from]!['bubbleColor']!;
+                final Color selectionColor = details[from]!['selectionColor']!;
+                final Color textColor = details[from]!['textColor']!;
+                final Color linkColor = details[from]!['linkColor']!;
 
                 if (message.isLessEmojisThan(5)) {
                   final EmojiParser parser = EmojiParser();
@@ -119,7 +119,7 @@ class ChatBubble extends StatelessWidget {
                   return EmojiMessageBubble(
                     message: message,
                     createdAt: createdAt,
-                    type: type,
+                    from: from,
                     status: status!,
                     fontSize: fontSize,
                   );
@@ -132,7 +132,7 @@ class ChatBubble extends StatelessWidget {
                   linkColor: linkColor,
                   message: message,
                   createdAt: createdAt,
-                  type: type,
+                  from: from,
                   status: status!,
                 );
               }),
