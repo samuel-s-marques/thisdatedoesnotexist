328acdde211ace00f44be0f043f25d362fe5eeba
Samuel Marques <samuel.s.marques@protonmail.com>
2024-01-31 17:52:06 -0300

    Refactor Message model to include content, location, and duration

--- lib/app/core/models/message_model.dart
+++ lib/app/core/models/message_model.dart
@@ -5,21 +5,25 @@ import 'package:thisdatedoesnotexist/app/features/chat/widgets/message_type_enum
 class Message {
   Message({
     this.id,
-    this.text,
+    this.content,
     this.from,
     this.type,
     this.sendBy,
     this.status,
+    this.location,
+    this.duration,
     this.createdAt,
   });
 
   factory Message.fromMap(Map<dynamic, dynamic> json) {
     return Message(
       id: json['id'],
-      text: json['text'],
+      content: json['content'],
       from: MessageFrom.values.firstWhere((MessageFrom from) => from.name == json['from']),
       type: MessageType.values.firstWhere((MessageType type) => type.name == json['type']),
       status: MessageStatus.values.firstWhere((MessageStatus status) => status.name == json['status']),
+      location: json['location'],
+      duration: json['duration'] != null ? Duration(seconds: json['duration']) : null,
       sendBy: json['send_by'],
       createdAt: json['created_at'] != null ? DateTime.parse(json['created_at']) : null,
     );
@@ -28,9 +32,11 @@ class Message {
   Map<String, dynamic> toMap() {
     return {
       'id': id,
-      'text': text,
+      'content': content,
       'from': from?.name,
       'type': type?.name,
+      'location': location,
+      'duration': duration?.inSeconds,
       'send_by': sendBy,
       'status': status?.name,
       'created_at': createdAt?.toIso8601String(),
@@ -38,10 +44,12 @@ class Message {
   }
 
   final String? id;
-  final String? text;
+  final dynamic content;
   final MessageFrom? from;
   final MessageType? type;
   final String? sendBy;
   final DateTime? createdAt;
+  final String? location;
+  final Duration? duration;
   MessageStatus? status;
 }
