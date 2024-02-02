46134c8bfc9f78d633d10b0cec6cfa5735941d9f
Samuel Marques <samuel.s.marques@protonmail.com>
2024-02-02 19:54:18 -0300

    Update amplitude parameter to amplitudeValues in WaveformView

--- lib/app/features/chat/widgets/waveform_view.dart
+++ lib/app/features/chat/widgets/waveform_view.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:thisdatedoesnotexist/app/features/chat/widgets/waveform_painter.dart';
 
 class WaveformView extends StatelessWidget {
-  const WaveformView({super.key, required this.amplitude});
+  const WaveformView({super.key, required this.amplitudeValues});
 
-  final double amplitude;
+  final List<double> amplitudeValues;
 
   @override
   Widget build(BuildContext context) {
     return CustomPaint(
-      painter: WaveformPainter(amplitude: amplitude),
+      painter: WaveformPainter(amplitudeValues: amplitudeValues),
     );
   }
 }
