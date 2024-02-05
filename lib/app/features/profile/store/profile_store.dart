d24238ab5f812db8ae7d3e2c296a45c1730aa3ed
Samuel Marques <samuel.s.marques@protonmail.com>
2024-02-05 17:06:28 -0300

    Refactor profile_store.dart

--- lib/app/features/profile/store/profile_store.dart
+++ lib/app/features/profile/store/profile_store.dart
@@ -1,22 +1,19 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
 import 'package:thisdatedoesnotexist/app/core/models/user_model.dart';
-import 'package:thisdatedoesnotexist/app/core/services/auth_service.dart';
-import 'package:thisdatedoesnotexist/app/core/services/database_service.dart';
+import 'package:thisdatedoesnotexist/app/features/profile/services/profile_service.dart';
 
 part 'profile_store.g.dart';
 
 class ProfileStore = ProfileStoreBase with _$ProfileStore;
 
 abstract class ProfileStoreBase with Store {
-  String server = const String.fromEnvironment('SERVER');
-  DatabaseService databaseService = DatabaseService();
-  AuthService authService = AuthService();
-  User? authenticatedUser;
+  final ProfileService service = Modular.get();
+  final String server = const String.fromEnvironment('SERVER');
 
   @action
-  Future<UserModel?> getUser() async {
-    authenticatedUser = authService.getUser();
-    return databaseService.getUser();
-  }
+  Future<UserModel?> getUser() => service.getUser();
+
+  @action
+  Future<void> updateUser(UserModel user) => service.updateUser(user);
 }
