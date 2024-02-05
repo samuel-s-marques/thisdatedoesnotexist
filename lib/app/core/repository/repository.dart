6ed5236b87c9b40dfcd881341523c97ce04d66eb
Samuel Marques <samuel.s.marques@protonmail.com>
2024-02-05 16:57:15 -0300

    Update repository methods to accept dynamic body

--- lib/app/core/repository/repository.dart
+++ lib/app/core/repository/repository.dart
@@ -3,8 +3,8 @@ import 'package:thisdatedoesnotexist/app/core/models/return_model.dart';
 
 abstract class Repository {
   Future<Return> get(String url, {HttpOptions? options});
-  Future<Return> post(String url, Map<String, dynamic> body, {HttpOptions? options});
-  Future<Return> put(String url, Map<String, dynamic> body, {HttpOptions? options});
+  Future<Return> post(String url, dynamic body, {HttpOptions? options});
+  Future<Return> put(String url, dynamic body, {HttpOptions? options});
   Future<Return> delete(String url, {HttpOptions? options});
   Future<Return> download(String url, String savePath, {HttpOptions? options});
 }
