b4632824db25eec31fb666b9b631e1e3100dec5a
Samuel Marques <samuel.s.marques@protonmail.com>
2024-02-05 16:59:09 -0300

    Add AuthService implementation

---
+++ lib/app/features/auth/services/auth_service.dart
@@ -0,0 +1,152 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
+import 'package:thisdatedoesnotexist/app/core/models/options.dart';
+import 'package:thisdatedoesnotexist/app/core/models/service_return_model.dart';
+import 'package:thisdatedoesnotexist/app/core/models/user_model.dart';
+import 'package:thisdatedoesnotexist/app/core/repository/repository.dart';
+import 'package:thisdatedoesnotexist/app/core/util.dart';
+import 'package:thisdatedoesnotexist/app/features/auth/services/database_service.dart';
+
+abstract class AuthService {
+  Future<ServiceReturn> createAccount({required String email, required String password});
+  Future<ServiceReturn> login({required String email, required String password});
+  Future<ServiceReturn> loginWithGoogle();
+  Future<ServiceReturn> logout();
+  Future<ServiceReturn> resetPassword({required String email});
+  Future<ServiceReturn> deleteAccount();
+  Future<bool> isAuthenticated();
+  Future<String?> getToken();
+  User? getUser();
+}
+
+class AuthServiceImpl implements AuthService {
+  final Repository _repository = Modular.get<Repository>();
+  final String _server = const String.fromEnvironment('SERVER');
+  FlutterSecureStorage storage = const FlutterSecureStorage();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final DatabaseService _databaseService = Modular.get<DatabaseService>();
+
+  @override
+  Future<ServiceReturn> createAccount({required String email, required String password}) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      await _auth.currentUser!.updateDisplayName(email.toUsername());
+      await storage.write(key: 'token', value: await _auth.currentUser!.getIdToken());
+      await storage.write(key: 'uid', value: _auth.currentUser!.uid);
+
+      return ServiceReturn(success: true);
+    } on FirebaseAuthException catch (e) {
+      return ServiceReturn(success: false, message: e.message);
+    }
+  }
+
+  @override
+  Future<ServiceReturn> deleteAccount() async {
+    try {
+      await _repository.delete('$_server/api/users', options: HttpOptions());
+
+      await _auth.currentUser!.delete();
+      await OneSignal.logout();
+      await storage.deleteAll();
+
+      return ServiceReturn(success: true);
+    } on FirebaseAuthException catch (e) {
+      return ServiceReturn(success: false, message: e.message);
+    }
+  }
+
+  @override
+  Future<ServiceReturn> login({required String email, required String password}) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      await storage.write(key: 'token', value: await _auth.currentUser!.getIdToken());
+      await storage.write(key: 'uid', value: _auth.currentUser!.uid);
+
+      return ServiceReturn(success: true);
+    } on FirebaseAuthException catch (e) {
+      return ServiceReturn(success: false, message: e.message);
+    }
+  }
+
+  @override
+  Future<ServiceReturn> loginWithGoogle() async {
+    try {
+      final googleUser = await GoogleSignIn().signIn();
+      final googleAuth = await googleUser!.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      await _auth.signInWithCredential(credential);
+      await storage.write(key: 'token', value: await _auth.currentUser!.getIdToken());
+      await storage.write(key: 'uid', value: _auth.currentUser!.uid);
+
+      return ServiceReturn(success: true);
+    } on FirebaseAuthException catch (e) {
+      return ServiceReturn(success: false, message: e.message);
+    }
+  }
+
+  @override
+  Future<ServiceReturn> logout() async {
+    try {
+      await _auth.signOut();
+      await OneSignal.logout();
+      await storage.deleteAll();
+
+      return ServiceReturn(success: true);
+    } on FirebaseAuthException catch (e) {
+      return ServiceReturn(success: false, message: e.message);
+    }
+  }
+
+  @override
+  Future<ServiceReturn> resetPassword({required String email}) async {
+    try {
+      await _auth.sendPasswordResetEmail(email: email.trim());
+
+      return ServiceReturn(success: true);
+    } on FirebaseAuthException catch (e) {
+      return ServiceReturn(success: false, message: e.message);
+    }
+  }
+
+  @override
+  User? getUser() {
+    return _auth.currentUser;
+  }
+
+  @override
+  Future<bool> isAuthenticated() async {
+    if (getUser() == null) {
+      return false;
+    }
+
+    final ServiceReturn response = await _databaseService.getUser();
+    final UserModel? user = response.data;
+
+    return user != null && user.active!;
+  }
+
+  @override
+  Future<String?> getToken() async {
+    if (await isAuthenticated()) {
+      return _auth.currentUser!.getIdToken();
+    }
+
+    return null;
+  }
+}
