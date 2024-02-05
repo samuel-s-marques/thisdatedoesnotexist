cb0aeba542ae6cdd96f100b91c902ac4ed23ce47
Samuel Marques <samuel.s.marques@protonmail.com>
2024-02-05 17:00:06 -0300

    Add HomeService and DataService singletons

--- lib/app/features/home/home_module.dart
+++ lib/app/features/home/home_module.dart
@@ -1,10 +1,14 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:thisdatedoesnotexist/app/core/services/data_service.dart';
 import 'package:thisdatedoesnotexist/app/features/home/pages/home_page.dart';
+import 'package:thisdatedoesnotexist/app/features/home/services/home_service.dart';
 import 'package:thisdatedoesnotexist/app/features/home/store/home_store.dart';
 
 class HomeModule extends Module {
   @override
   void binds(i) {
+    i.addSingleton<HomeService>(HomeServiceImpl.new);
+    i.addSingleton<DataService>(DataServiceImpl.new);
     i.addLazySingleton((i) => HomeStore());
   }
 
