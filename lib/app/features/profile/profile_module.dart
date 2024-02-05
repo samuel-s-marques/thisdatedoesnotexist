5351c059c92a7f45a5463a09fca519ffd9d36546
Samuel Marques <samuel.s.marques@protonmail.com>
2024-02-05 17:06:25 -0300

    Add ProfileService to ProfileModule

--- lib/app/features/profile/profile_module.dart
+++ lib/app/features/profile/profile_module.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:thisdatedoesnotexist/app/core/widgets/widget_module.dart';
 import 'package:thisdatedoesnotexist/app/features/profile/pages/profile_page.dart';
+import 'package:thisdatedoesnotexist/app/features/profile/services/profile_service.dart';
 import 'package:thisdatedoesnotexist/app/features/profile/store/profile_store.dart';
 
 class ProfileModule extends WidgetModule {
@@ -9,6 +10,7 @@ class ProfileModule extends WidgetModule {
 
   @override
   void binds(Injector i) {
+    i.addSingleton<ProfileService>(ProfileServiceImpl.new);
     i.addLazySingleton((i) => ProfileStore());
   }
 
