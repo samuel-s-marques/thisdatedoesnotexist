9a88c2a9f59000ec994ce2d499f2293751007013
Samuel Marques <samuel.s.marques@protonmail.com>
2024-02-05 17:05:32 -0300

    Add NotificationService implementation

---
+++ lib/app/features/notification/services/notification_service.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:thisdatedoesnotexist/app/core/models/options.dart';
+import 'package:thisdatedoesnotexist/app/core/models/return_model.dart';
+import 'package:thisdatedoesnotexist/app/core/models/service_return_model.dart';
+import 'package:thisdatedoesnotexist/app/core/repository/repository.dart';
+
+abstract class NotificationService {
+  Future<ServiceReturn> getNotifications();
+  Future<ServiceReturn> getReportedMessages();
+}
+
+class NotificationServiceImpl implements NotificationService {
+  final Repository _repository = Modular.get<Repository>();
+  final String _server = const String.fromEnvironment('SERVER');
+  FlutterSecureStorage storage = const FlutterSecureStorage();
+
+  @override
+  Future<ServiceReturn> getNotifications() async {
+    final Return response = await _repository.get(
+      '$_server/api/notifications',
+      options: HttpOptions(cache: false),
+    );
+
+    if (response.statusCode == 200) {
+      return ServiceReturn(
+        success: true,
+        data: response.data,
+      );
+    }
+
+    return ServiceReturn(success: false);
+  }
+
+  @override
+  Future<ServiceReturn> getReportedMessages() async {
+    final Return response = await _repository.get(
+      '$_server/api/status/account',
+      options: HttpOptions(cache: false),
+    );
+
+    if (response.statusCode == 200) {
+      return ServiceReturn(
+        success: true,
+        data: response.data,
+      );
+    }
+
+    return ServiceReturn(success: false);
+  }
+}
