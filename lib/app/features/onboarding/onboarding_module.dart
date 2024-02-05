5a4cdc20875687f44af4a6ea2adf75573ccdad08
Samuel Marques <samuel.s.marques@protonmail.com>
2024-02-05 17:05:55 -0300

    Add OnboardingService and DataService singletons

--- lib/app/features/onboarding/onboarding_module.dart
+++ lib/app/features/onboarding/onboarding_module.dart
@@ -1,10 +1,14 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:thisdatedoesnotexist/app/core/services/data_service.dart';
 import 'package:thisdatedoesnotexist/app/features/onboarding/pages/onboarding_page.dart';
+import 'package:thisdatedoesnotexist/app/features/onboarding/services/onboarding_service.dart';
 import 'package:thisdatedoesnotexist/app/features/onboarding/store/onboarding_store.dart';
 
 class OnboardingModule extends Module {
   @override
   void binds(i) {
+    i.addSingleton<OnboardingService>(OnboardingServiceImpl.new);
+    i.addSingleton<DataService>(DataServiceImpl.new);
     i.addLazySingleton((i) => OnboardingStore());
   }
 
