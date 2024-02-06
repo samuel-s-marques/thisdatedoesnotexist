d7ae482111d2adbda2556cb913903bc68e86c5ad
Samuel Marques <samuel.s.marques@protonmail.com>
2024-02-06 14:13:01 -0300

    Fix response status code in OnboardingServiceImpl

--- lib/app/features/onboarding/services/onboarding_service.dart
+++ lib/app/features/onboarding/services/onboarding_service.dart
@@ -27,7 +27,7 @@ class OnboardingServiceImpl implements OnboardingService {
         options: HttpOptions(),
       );
 
-      if (response.statusCode == 201) {
+      if (response.statusCode == 200) {
         return ServiceReturn(
           success: true,
           data: response.data,
