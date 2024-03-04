7e4dec723485b8111eef62ea60a059747847c0a6
Samuel Marques <samuel.s.marques@protonmail.com>
2024-03-04 08:30:21 -0300

    Update user authentication logic

--- lib/app/features/profile/pages/edit_profile_page.dart
+++ lib/app/features/profile/pages/edit_profile_page.dart
@@ -2,16 +2,29 @@ import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:intl/intl.dart';
 import 'package:thisdatedoesnotexist/app/core/models/base_model.dart';
+import 'package:thisdatedoesnotexist/app/core/models/pronoun_model.dart';
 import 'package:thisdatedoesnotexist/app/core/util.dart';
 import 'package:thisdatedoesnotexist/app/core/widgets/searchable_list_view.dart';
 import 'package:thisdatedoesnotexist/app/features/profile/store/profile_store.dart';
 
-class EditProfilePage extends StatelessWidget {
+class EditProfilePage extends StatefulWidget {
   EditProfilePage({super.key});
 
+  @override
+  State<EditProfilePage> createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
   final ProfileStore store = Modular.get<ProfileStore>();
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    store.updatedUser = store.user;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +35,9 @@ class EditProfilePage extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.check),
             onPressed: () {
-              if (formKey.currentState!.validate()) {}
+              if (formKey.currentState!.validate()) {
+                store.updateUser();
+              }
             },
           ),
         ],
@@ -39,8 +54,15 @@ class EditProfilePage extends StatelessWidget {
             children: [
               const Text('Enter your name'),
               TextFormField(
-                controller: store.nameController,
+                controller: TextEditingController(
+                  text: store.user?.name,
+                ),
                 textCapitalization: TextCapitalization.words,
+                onChanged: (value) {
+                  if (value.trim().isNotEmpty) {
+                    store.updatedUser = store.updatedUser!.copyWith(name: value.trim());
+                  }
+                },
                 validator: (value) {
                   if (value!.isEmpty) {
                     return 'Please enter your name.';
@@ -55,7 +77,14 @@ class EditProfilePage extends StatelessWidget {
               const SizedBox(height: 15),
               const Text('Enter your surname'),
               TextFormField(
-                controller: store.surnameController,
+                controller: TextEditingController(
+                  text: store.user?.surname,
+                ),
+                onChanged: (value) {
+                  if (value.trim().isNotEmpty) {
+                    store.updatedUser = store.updatedUser!.copyWith(surname: value.trim());
+                  }
+                },
                 textCapitalization: TextCapitalization.words,
                 validator: (value) {
                   if (value!.isEmpty) {
@@ -72,7 +101,9 @@ class EditProfilePage extends StatelessWidget {
               const Text('Your birthday'),
               TextField(
                 readOnly: true,
-                controller: store.birthdayController,
+                controller: TextEditingController(
+                  text: DateFormat('dd/MM/yyyy').format(store.user!.birthDay!),
+                ),
                 onTap: () => store.selectBirthday(context),
               ),
               const SizedBox(height: 15),
@@ -88,7 +119,15 @@ class EditProfilePage extends StatelessWidget {
                 children: [
                   Expanded(
                     child: TextFormField(
-                      controller: store.heightController,
+                      controller: TextEditingController(
+                        text: store.user?.height?.toString() ?? '1.66',
+                      ),
+                      onChanged: (value) {
+                        if (value.trim().isNotEmpty) {
+                          final double height = double.parse(value.trim().replaceAll(',', '.'));
+                          store.updatedUser = store.updatedUser!.copyWith(height: height);
+                        }
+                      },
                       keyboardType: TextInputType.number,
                       decoration: const InputDecoration(
                         suffixText: 'm',
@@ -106,7 +145,15 @@ class EditProfilePage extends StatelessWidget {
                   const SizedBox(width: 10),
                   Expanded(
                     child: TextFormField(
-                      controller: store.weightController,
+                      controller: TextEditingController(
+                        text: store.user?.weight?.toString() ?? '60',
+                      ),
+                      onChanged: (value) {
+                        if (value.trim().isNotEmpty) {
+                          final double weight = double.parse(value.trim().replaceAll(',', '.'));
+                          store.updatedUser = store.updatedUser!.copyWith(weight: weight);
+                        }
+                      },
                       keyboardType: TextInputType.number,
                       decoration: const InputDecoration(
                         suffixText: 'kg',
@@ -126,9 +173,16 @@ class EditProfilePage extends StatelessWidget {
               const SizedBox(height: 15),
               const Text('Enter your bio'),
               TextFormField(
-                controller: store.bioController,
+                controller: TextEditingController(
+                  text: store.user?.bio ?? '',
+                ),
                 maxLength: 500,
                 maxLines: null,
+                onChanged: (value) {
+                  if (value.trim().isNotEmpty) {
+                    store.updatedUser = store.updatedUser!.copyWith(bio: value.trim());
+                  }
+                },
                 textCapitalization: TextCapitalization.sentences,
                 decoration: const InputDecoration(
                   hintText: 'Your bio',
@@ -146,7 +200,7 @@ class EditProfilePage extends StatelessWidget {
                       child: Text(name),
                     );
                   }).toList(),
-                  value: store.sex,
+                  value: store.user?.sex ?? BaseModel(id: 1, name: 'male'),
                   validator: (value) {
                     if (value == null) {
                       return 'Please enter your sex.';
@@ -167,7 +221,14 @@ class EditProfilePage extends StatelessWidget {
                       child: Text('${pronoun.subjectPronoun}/${pronoun.objectPronoun}'),
                     );
                   }).toList(),
-                  value: store.selectedPronouns,
+                  value: store.user?.pronoun ??
+                      Pronoun(
+                        objectPronoun: 'they',
+                        possessiveAdjective: 'their',
+                        possessivePronoun: 'theirs',
+                        subjectPronoun: 'them',
+                        type: 'neutral',
+                      ),
                   validator: (value) {
                     if (value == null) {
                       return 'Please pick your pronouns.';
@@ -190,7 +251,7 @@ class EditProfilePage extends StatelessWidget {
                       child: Text(name.capitalize()),
                     );
                   }).toList(),
-                  value: store.religion,
+                  value: store.user?.religion ?? BaseModel(id: 1, name: 'Islam'),
                   validator: (value) {
                     if (value == null) {
                       return 'Please enter your religion.';
@@ -213,7 +274,7 @@ class EditProfilePage extends StatelessWidget {
                       child: Text(name.capitalize()),
                     );
                   }).toList(),
-                  value: store.selectedRelationshipGoal,
+                  value: store.user?.relationshipGoal ?? BaseModel(id: 1, name: 'casual'),
                   validator: (value) {
                     if (value == null) {
                       return 'Please enter your relationship goal.';
@@ -228,7 +289,6 @@ class EditProfilePage extends StatelessWidget {
               const Text('Enter your political view'),
               Observer(
                 builder: (_) => DropdownButtonFormField<BaseModel>(
-                  value: store.selectedPoliticalView,
                   items: store.politicalViews.map((view) {
                     final String name = view.name!;
 
@@ -237,6 +297,7 @@ class EditProfilePage extends StatelessWidget {
                       child: Text(name.capitalize()),
                     );
                   }).toList(),
+                  value: store.user?.politicalView ?? BaseModel(id: 1, name: 'Far left'),
                   validator: (value) {
                     if (value == null) {
                       return 'Please enter your political view.';
@@ -252,7 +313,9 @@ class EditProfilePage extends StatelessWidget {
               Observer(
                 builder: (_) => TextFormField(
                   readOnly: true,
-                  controller: store.occupationController,
+                  controller: TextEditingController(
+                    text: store.user?.occupation?.name ?? 'Select an occupation',
+                  ),
                   onTap: () {
                     showDialog(
                       context: context,
@@ -281,7 +344,7 @@ class EditProfilePage extends StatelessWidget {
                 showFlagMain: true,
                 showFlagDialog: true,
                 alignLeft: true,
-                initialSelection: store.selectedCountry,
+                initialSelection: store.user?.country ?? 'Brasil',
               ),
             ],
           ),
